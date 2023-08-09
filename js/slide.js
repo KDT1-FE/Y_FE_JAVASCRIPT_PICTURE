0b3e87b87db7e846eaa08402ccaafd2272bd9891
Leejinuk <leejin7900@gmail.com>
2023-08-09 16:01:37 +0900

    Feat: 메인페이지구성 및 틀짜기

---
+++ js/slide.js
@@ -0,0 +1,16 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("mySlides");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 5000);
+}
