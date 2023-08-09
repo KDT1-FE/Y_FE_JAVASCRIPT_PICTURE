c2dd6f9fec06f9cd2a95627a69f27a536525acea
gaori <monooi@naver.com>
2023-08-09 16:32:21 +0900

    Feat: 유저 리스트 레이아웃 및 업로드 리팩토링

--- src/upload.js
+++ src/upload.js
@@ -2,12 +2,22 @@ import { uploadImageToStorage, uploadInfoToDatabase } from "./uploadImage";
 
 const form = document.querySelector(".form");
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  uploadImageToStorage().then((imageUrl) => {
-    if (imageUrl) {
-      uploadInfoToDatabase(imageUrl);
-    }
-  });
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const rank = document.getElementById("rank").value;
+  const imageFile = document.getElementById("chooseFile").files[0];
+
+  const imageUrl = await uploadImageToStorage(imageFile);
+
+  const userData = {
+    name,
+    email,
+    rank,
+    imageUrl,
+  };
+
+  uploadInfoToDatabase(userData);
 });
--- src/uploadImage.js
+++ src/uploadImage.js
@@ -2,15 +2,13 @@ import { uploadBytes, getDownloadURL, ref } from "firebase/storage";
 import { db, storage } from "./firebase";
 import { addDoc, collection } from "firebase/firestore";
 
-export function uploadImageToStorage() {
+export function uploadImageToStorage(imageFile) {
   return new Promise((resolve, reject) => {
-    const imageInput = document.getElementById("chooseFile");
-    const imageUrl = imageInput.files[0];
-    const uniqueImageUrl = new Date().getTime() + "-" + imageUrl.name;
+    const uniqueImageUrl = new Date().getTime() + "-" + imageFile.name;
 
     const storageRef = ref(storage, "profile_images/" + uniqueImageUrl);
     // 스토리지에 이미지 업로드 및 URL 생성
-    uploadBytes(storageRef, imageUrl)
+    uploadBytes(storageRef, imageFile)
       .then(async (snapshot) => {
         console.log("Uploaded successfully");
         getDownloadURL(storageRef).then((downloadURL) => {
@@ -25,27 +23,9 @@ export function uploadImageToStorage() {
   });
 }
 
-export async function uploadInfoToDatabase(imageUrl) {
-  const nameInput = document.getElementById("name");
-  const emailInput = document.getElementById("email");
-  const rankInput = document.getElementById("rank");
-
-  const name = nameInput.value;
-  const email = emailInput.value;
-  const rank = rankInput.value;
-
-  // form validation later
-
-  const data = {
-    name,
-    email,
-    rank,
-    // how to put imageUrl here
-    imageUrl,
-  };
-
+export async function uploadInfoToDatabase(userData) {
   try {
-    await addDoc(collection(db, "users"), data);
+    await addDoc(collection(db, "users"), userData);
     console.log("Added successfully");
   } catch (err) {
     console.error(err);
