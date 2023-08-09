57ecb57648624bdf5d4cd1b50adca8ae4a4592c6
89882 <pgh123166@naver.com>
2023-08-09 18:35:55 +0900

    feat: member 경로 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,12 @@
 import Home from './Home';
+import Member from './Member';
 export const routes = [
   {
     path: '/',
     component: Home,
+  },
+  {
+    path: '/member',
+    component: Member,
   },
 ];
