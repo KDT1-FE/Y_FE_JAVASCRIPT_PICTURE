60bcc6337e7031f809462c8ed56e2e223b3f1aaf
junkue20 <junkue17@naver.com>
2023-08-09 10:02:56 +0900

    Feat: 로딩화면 CSS 및 JS 구현

---
+++ js/loading.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+    const loadingScreen = document.querySelector(".loading-screen");
+
+    // 로딩이 완료되면 로딩 화면을 천천히 사라지게 함
+    setTimeout(() => {
+      loadingScreen.style.opacity = "0";
+      setTimeout(() => {
+        loadingScreen.style.display = "none";
+      }, 500); // 로딩 화면이 완전히 사라지도록 딜레이 설정
+    }, 1000); // 로딩 화면이 보이는 시간
+  };
