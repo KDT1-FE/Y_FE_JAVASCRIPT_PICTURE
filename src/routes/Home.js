6455d366c49acd114126b81c1b9b55f3c4ab9e90
89882 <pgh123166@naver.com>
2023-08-09 17:02:35 +0900

    feat: 테이블 제목 구현

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -9,6 +9,15 @@ export default class Home extends Component {
       <button class="button-home add-person">등록</button>
       <button class="button-home delete-person">삭제</button>
     </div>
-    </header>`;
+    </header>
+    <div class='table'>
+  <div class='checkbox-container'>
+    <input class='checkbox' type='checkbox'></input>
+  </div>
+  <p class='photo-title'>PHOTO</p>
+  <p class="name-title">NAME</p>
+  <p class="email-title">EMAIL</p>
+</div>
+    `;
   }
 }
