d250d17f3605568ce6f6709d17090bec85da1f59
89882 <pgh123166@naver.com>
2023-08-09 17:08:43 +0900

    feat: store 구현

---
+++ src/core/store.js
@@ -0,0 +1,25 @@
+export class Store {
+  constructor(state) {
+    this.state = {}; // 상태(데이터)
+    this.observers = {};
+    for (const key in state) {
+      Object.defineProperty(this.state, key, {
+        // Getter
+        get: () => state[key],
+        // Setter
+        set: (val) => {
+          state[key] = val;
+          if (Array.isArray(this.observers[key])) {
+            // 호출할 콜백이 있는 경우!
+            this.observers[key].forEach((observer) => observer(val));
+          }
+        },
+      });
+    }
+  }
+  subscribe(key, cb) {
+    Array.isArray(this.observers[key])
+      ? this.observers[key].push(cb)
+      : (this.observers[key] = [cb]);
+  }
+}
