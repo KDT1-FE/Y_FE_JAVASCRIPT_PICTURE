2257ccc2aeb98dbab48f0d83180c1e3671627949
SOL-MI <ovo10203@gmail.com>
2023-08-10 16:39:43 +0900

    Feat: 이미지 및 텍스트 firebase 업로드 기능 구현

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-analytics.js";
 import { getStorage, ref, uploadBytes, getDownloadURL} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-storage.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
+import { getFirestore, collection, addDoc, getDocs, orderBy, query } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -18,61 +18,103 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
 const storage = getStorage(app);
-const storageRef = ref(storage,'hello'); //사진 업로드...관련
-const pathReference  = ref(storage, 'images/IMG-0500.png');
+const db = getFirestore(app);
 
-//사진 업로드하기
-let inputfile = document.getElementById("avatar");
-console.log(inputfile);
 
-inputfile.addEventListener('change', showImage);
+//선택 이미지 화면에 띄우기
+const imgFileInput = document.querySelector('#avatar');
+imgFileInput.addEventListener('change', showImage);
+
+//저장 버튼 : 텍스트 -> db 업로드 && 이미지 -> storage 업로드
+document.querySelector('.btn__upload').addEventListener('click', function(){
+  const profileContent = {
+    name: document.querySelector('.profile__name').value,
+    content: document.querySelector('.profile__introduce').value,
+    position: document.querySelector('.profile__position').value,
+  };
+  if(profileContent.name && profileContent.content && profileContent.position && imgFileInput && imgFileInput.files.length>0){
+    addDoc(collection(db, 'profiles'), profileContent)
+    .then(() => {
+      console.log('Document successfully written!');
+    })
+    .catch((error) => {
+      console.error('Error writing document: ', error);
+    });
+    uploadImgToStorage();
+  }else{
+    if(imgFileInput.files.length<=0){
+      console.log("이미지를 선택해주세요!");
+    }else{
+      console.log('텍스트를 입력해주세요!');
+    }
+  }
+})
 
 function showImage(){
-  const selectedFile = inputfile.files[0];
+  const selectedFile = imgFileInput.files[0];
   console.log(selectedFile)
   const file = URL.createObjectURL(selectedFile);
   document.querySelector(".preview").src = file;
 
-  uploadBytes(storageRef, inputfile).then((snapshot) => {
-    console.log('Uploaded a blob or file!');
-  });
+  // const storageRef = ref(storage, `images/${selectedFile.name}`);
+  // uploadBytes(storageRef, file).then((snapshot) => {
+  //   console.log('Uploaded a blob or file!');
+  // });
 }
 
+async function uploadImgToStorage(){
+  if(imgFileInput && imgFileInput.files.length>0){
+    const imgFile = imgFileInput.files[0];
+    const storageRef = ref(storage, 'image/'+imgFile.name);
+    const uploadTask = uploadBytes(storageRef, imgFile);
+
+    try {
+      const snapshot = await uploadTask;
+      const url = await getDownloadURL(snapshot.ref);
+      const toSave = {
+        date: new Date(),
+        image: url,
+      };
+      await addDoc(collection(db, 'profileImg'), toSave);
+      window.location.href = '/index.html';
+    } catch (error) {
+      console.log('실패사유는:', error);
+    }
+  } else {
+    console.log('파일을 선택하세요.');
+  }
+}
 
 //사진 불러오기
-getDownloadURL(pathReference)
-  .then((url) => {
-    //This can be downloaded directly:
-    const xhr = new XMLHttpRequest();
-    xhr.responseType = 'blob';
-    xhr.onload = (event) => {
-      const blob = xhr.response;
-    };
-    xhr.open('GET', url);
-    xhr.send();
+// getDownloadURL(pathReference)
+//   .then((url) => {
+//     //This can be downloaded directly:
+//     const xhr = new XMLHttpRequest();
+//     xhr.responseType = 'blob';
+//     xhr.onload = (event) => {
+//       const blob = xhr.response;
+//     };
+//     xhr.open('GET', url);
+//     xhr.send();
 
-    // Or inserted into an <img> element
-    const img = document.getElementById('myimg');
-    img.setAttribute('src', url);
-  })
-  .catch((error) => {
-    // Handle any errors
-  });
+//     // Or inserted into an <img> element
+//     const img = document.getElementById('myimg');
+//     img.setAttribute('src', url);
+//   })
+//   .catch((error) => {
+//     // Handle any errors
+//   });
 
 
 //db 추가/불러오기
-const db = getFirestore(app);
-
-
-try {
-  const docRef = await addDoc(collection(db, "users"), {
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815
-  });
-  console.log("Document written with ID: ", docRef.id);
-} catch (e) {
-  console.error("Error adding document: ", e);
-}
+// try {
+//   const docRef = await addDoc(collection(db, "users"), {
+//     first: "Ada",
+//     last: "Lovelace",
+//     born: 1815
+//   });
+//   console.log("Document written with ID: ", docRef.id);
+// } catch (e) {
+//   console.error("Error adding document: ", e);
+// }
