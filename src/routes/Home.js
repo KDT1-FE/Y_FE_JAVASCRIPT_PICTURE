d2ace1cf59c5e702126f6be55532a9949d8a40e7
89882 <pgh123166@naver.com>
2023-08-10 20:48:38 +0900

    refactor: add,delete 버튼 클래스명 수정

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import MemberList from '../components/MemberList';
 import { Component } from '../core/component';
+import { routeRender } from '../core/router';
 import { renderMemberList } from '../store/memberStore';
 
 export default class Home extends Component {
@@ -8,12 +9,12 @@ export default class Home extends Component {
     <div class="title">직원 관리 시스템</div>
     <div class="container">
       <input class="search" placeholder="이름으로 검색해주세요"/>
-      <button class="button-home" id="add-member">등록</button>
-      <button class="button-home" id ="delete-member">삭제</button>
+      <button class="button-home" id="navigate-write">등록</button>
+      <button class="button-home" id ="navigate-del">삭제</button>
     </div>
     </header>
     `;
-    await renderMemberList(); // renderMemberList 함수를 통해 store 업데이트 (store에 memberList를 담음)
+    // await renderMemberList(); // renderMemberList 함수를 통해 store 업데이트 (store에 memberList를 담음)
     const memberList = new MemberList().el;
     this.el.append(memberList);
     //memberList 컴포넌트 등록
@@ -25,7 +26,7 @@ export default class Home extends Component {
     });
     //title 클릭 시 Home 페이지로 이동
 
-    const addButton = this.el.querySelector('#add-member');
+    const addButton = this.el.querySelector('#navigate-write');
     addButton.addEventListener('click', () => {
       window.history.pushState(null, null, '/write');
       routeRender();
