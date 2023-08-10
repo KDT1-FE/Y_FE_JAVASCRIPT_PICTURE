8203a45745edbf70f91520618665fe5e9bb96329
SOL-MI <ovo10203@gmail.com>
2023-08-10 18:00:59 +0900

    Refactor: 데이터 업로드 코드 수정

--- index.js
+++ index.js
@@ -26,22 +26,31 @@ const db = getFirestore(app);
 const imgFileInput = document.querySelector('#avatar');
 imgFileInput.addEventListener('change', showImage);
 
-//저장 버튼 : 텍스트 -> db 업로드 && 이미지 -> storage 업로드
-document.querySelector('.btn__upload').addEventListener('click', function(){
-  const profileContent = {
+//저장 버튼 : 텍스트&이미지url -> db 업로드 && 이미지 -> storage 업로드
+document.querySelector('.btn__upload').addEventListener('click', async function(){
+  let profileContent = {
     name: document.querySelector('.profile__name').value,
     content: document.querySelector('.profile__introduce').value,
     position: document.querySelector('.profile__position').value,
   };
   if(profileContent.name && profileContent.content && profileContent.position && imgFileInput && imgFileInput.files.length>0){
-    addDoc(collection(db, 'profiles'), profileContent)
-    .then(() => {
-      console.log('Document successfully written!');
-    })
-    .catch((error) => {
+    try {
+      const imgFile = imgFileInput.files[0];
+      const storageRef = ref(storage, 'image/'+imgFile.name);
+      const uploadTask = uploadBytes(storageRef, imgFile);
+      const snapshot = await uploadTask;
+      const url = await getDownloadURL(snapshot.ref);
+      const toSave = {
+        date: new Date(),
+        image: url
+      };
+      Object.assign(profileContent,toSave);
+      await addDoc(collection(db, 'profiles'), profileContent);
+      window.location.href = '/index.html';
+      console.log('Document successfully written!')
+    }catch{(error) => {
       console.error('Error writing document: ', error);
-    });
-    uploadImgToStorage();
+    }};
   }else{
     if(imgFileInput.files.length<=0){
       console.log("이미지를 선택해주세요!");
@@ -56,38 +65,11 @@ function showImage(){
   console.log(selectedFile)
   const file = URL.createObjectURL(selectedFile);
   document.querySelector(".preview").src = file;
-
-  // const storageRef = ref(storage, `images/${selectedFile.name}`);
-  // uploadBytes(storageRef, file).then((snapshot) => {
-  //   console.log('Uploaded a blob or file!');
-  // });
 }
 
-async function uploadImgToStorage(){
-  if(imgFileInput && imgFileInput.files.length>0){
-    const imgFile = imgFileInput.files[0];
-    const storageRef = ref(storage, 'image/'+imgFile.name);
-    const uploadTask = uploadBytes(storageRef, imgFile);
-
-    try {
-      const snapshot = await uploadTask;
-      const url = await getDownloadURL(snapshot.ref);
-      const toSave = {
-        date: new Date(),
-        image: url,
-      };
-      await addDoc(collection(db, 'profileImg'), toSave);
-      window.location.href = '/index.html';
-    } catch (error) {
-      console.log('실패사유는:', error);
-    }
-  } else {
-    console.log('파일을 선택하세요.');
-  }
-}
 
 //사진 불러오기
-// getDownloadURL(pathReference)
+// getDownloadURL(ref(storage, 'image/싸인.png'))
 //   .then((url) => {
 //     //This can be downloaded directly:
 //     const xhr = new XMLHttpRequest();
@@ -99,12 +81,12 @@ async function uploadImgToStorage(){
 //     xhr.send();
 
 //     // Or inserted into an <img> element
-//     const img = document.getElementById('myimg');
-//     img.setAttribute('src', url);
-//   })
-//   .catch((error) => {
-//     // Handle any errors
-//   });
+  //   const img = document.getElementById('myimg');
+  //   img.setAttribute('src', url);
+  // })
+  // .catch((error) => {
+  //   // Handle any errors
+  // });
 
 
 //db 추가/불러오기
