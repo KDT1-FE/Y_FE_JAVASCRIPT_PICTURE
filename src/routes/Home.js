27f8c3aadff0a3740176c4cee233572a5c16b025
89882 <pgh123166@naver.com>
2023-08-10 14:53:11 +0900

    feat: 등록 버튼 클릭 함수 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -8,17 +8,26 @@ export default class Home extends Component {
     <div class="title">직원 관리 시스템</div>
     <div class="container">
       <input class="search" placeholder="이름으로 검색해주세요"/>
-      <button class="button-home add-person">등록</button>
-      <button class="button-home delete-person">삭제</button>
+      <button class="button-home" id="add-member">등록</button>
+      <button class="button-home" id ="delete-member">삭제</button>
     </div>
     </header>
     `;
-    await renderMemberList();
+    await renderMemberList(); // renderMemberList 함수를 통해 store 업데이트 (store에 memberList를 담음)
     const memberList = new MemberList().el;
     this.el.append(memberList);
+    //memberList 컴포넌트 등록
+
     const title = this.el.querySelector('.title');
     title.addEventListener('click', () => {
       window.history.pushState(null, null, '/');
+      routeRender();
+    });
+    //title 클릭 시 Home 페이지로 이동
+
+    const addButton = this.el.querySelector('#add-member');
+    addButton.addEventListener('click', () => {
+      window.history.pushState(null, null, '/write');
       routeRender();
     });
   }
