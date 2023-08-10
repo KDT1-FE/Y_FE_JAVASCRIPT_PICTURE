478502b07ebba5ae47861559d1023b7e2770e6ca
xxxjinn <tlsguswls319@naver.com>
2023-08-10 16:30:38 +0900

    feat: 해당 주민 상세 프로필로 이동하도록 수정

--- public/src/navigation.js
+++ public/src/navigation.js
@@ -6,3 +6,12 @@ villagerElements.forEach((villagerElement) => {
     window.location.href = `profile.html?id=${villagerId}`;
   });
 });
+
+//동적으로 만들어지는 villager에 대해 event 만들어주기
+document.querySelector(".villagers-list").addEventListener("click", (event) => {
+  const villagerElement = event.target.closest(".villager");
+  if (villagerElement) {
+    const villagerId = villagerElement.getAttribute("data-id");
+    window.location.href = `profile.html?id=${villagerId}`;
+  }
+});
