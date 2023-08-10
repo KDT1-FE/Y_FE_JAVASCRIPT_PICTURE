004bde53e2103dbe5614ff62819bf77422c9d5dd
89882 <pgh123166@naver.com>
2023-08-10 11:20:20 +0900

    design: member 페이지 css 구현

--- src/routes/Member.js
+++ src/routes/Member.js
@@ -1,5 +1,5 @@
 import { Component } from '../core/component';
-import { getUrlParam } from '../core/router';
+import { getUrlParam, routeRender } from '../core/router';
 import { getMemberDetail, memberStore } from '../store/memberStore';
 
 export default class Member extends Component {
@@ -8,23 +8,31 @@ export default class Member extends Component {
     getMemberDetail(id);
     const member = memberStore.state.member;
     this.el.innerHTML = `
+    <header class="header">
+  <div class="title">직원 관리 시스템</div></header>
   <main class="detail">
     <div class='photo'></div>
     <section class='information-container'>
       <section class='information-title'>
-        <div class='information-text'>INFORMATION</div>
-        <button class='button'>정보 수정</button>
+        INFORMATION
+        <button class='button'>수정</button>
       </section>
       <p class='information'>
-        <div class='name-detail'>NAME</div>
-        <div>${member.name}</div>
+        <span class='detail-category'>NAME</span>
+        <span class="detail-value">${member.name}</span>
       </p>
       <p class='information'>
-        <div class='name-email'>EMAIL</div>
-        <div>${member.email}</div>
+        <span class='detail-category'>EMAIL</span>
+        <span class="detail-value">${member.email}</span>
       </p>
     </section>
   </main> 
     `;
+
+    const title = this.el.querySelector('.title');
+    title.addEventListener('click', () => {
+      window.history.pushState(null, null, '/');
+      routeRender();
+    });
   }
 }
