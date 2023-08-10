8bcae945919b85e0e6717cd635a6359b84744ef1
89882 <pgh123166@naver.com>
2023-08-10 12:37:42 +0900

    feat: firebase에서 데이터 read 구현

--- src/store/memberStore.js
+++ src/store/memberStore.js
@@ -1,19 +1,31 @@
 import { Store } from '../core/store';
 import { data } from '../../test';
+import { db } from '../api/firebase';
+import { collection, getDocs, query, where } from 'firebase/firestore';
 export const memberStore = new Store({
   members: [],
   loading: false,
   member: {},
 });
-export const renderMemberList = () => {
-  memberStore.state.loading = true;
-  console.log(data);
-  const { members } = data;
-  memberStore.state.members = [...members];
-  memberStore.state.loading = false;
+export const renderMemberList = async () => {
+  // memberStore.state.loading = true;
+  const res = await getDocs(collection(db, 'list'));
+  let array = [];
+  res.forEach((doc) => {
+    let memberData = doc.data();
+    console.log(memberData);
+    array.push({
+      name: memberData.name,
+      photoUrl: memberData.photoUrl,
+      email: memberData.email,
+      id: doc.id,
+    });
+  });
+  memberStore.state.members = [...array];
+  // memberStore.state.loading = false;
 };
 
-export const getMemberDetail = (id) => {
+export const getMemberDetail = async (id) => {
   const { member } = data;
   memberStore.state.member = member;
 };
