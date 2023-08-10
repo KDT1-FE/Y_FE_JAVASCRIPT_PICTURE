c9a3d410463810cc529374ca5e342cdd64b6e2f7
89882 <pgh123166@naver.com>
2023-08-10 20:47:18 +0900

    feat: 작성 페이지 구현

---
+++ src/routes/Write.js
@@ -0,0 +1,74 @@
+import { addDoc, collection, collectionGroup } from 'firebase/firestore';
+import { db, storage } from '../api/firebase';
+import { Component } from '../core/component';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import { v4 as uuidv4 } from 'uuid';
+import { routeRender } from '../core/router';
+
+export default class Write extends Component {
+  render() {
+    this.el.innerHTML = `
+    <header class="header">
+    <div class="title">직원 관리 시스템</div></header>
+    <section class="write-title">
+    직원을 등록해주세요
+    </section>
+    <form class="write-container" id='form'>
+    <input class="write-name" placeholder="이름을 입력해주세요" name="name"/>
+    <input class="write-email" placeholder="이메일을 입력해주세요" name="email"/>
+    <div class="image-container">
+    <input class="write-image" value='' placeholder="이미지를 첨부해주세요" disabled>
+    <label for="file" class="file-label">파일 선택</label> 
+    <input type="file" name="file" id="file" accept=".jpg, .png" class="file-input">
+    </div>
+    <button class="add-member" type="submit">등록</button>
+    </form>
+        `;
+
+    const imageFile = this.el.querySelector('.file-input');
+    const writeImage = this.el.querySelector('.write-image');
+    imageFile.addEventListener('change', () => {
+      writeImage.value = imageFile.value;
+    });
+    const form = this.el.querySelector('.write-container');
+
+    const uploadImage = async (fileData) => {
+      const storageRef = ref(storage, uuidv4());
+      await uploadBytes(storageRef, fileData);
+      return storageRef;
+    };
+    const uploadData = async (data) => {
+      const docRef = await addDoc(collection(db, 'list'), {
+        name: data.name,
+        email: data.email,
+        photoUrl: data.photoUrl,
+      });
+      console.log(docRef);
+    };
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      const formData = new FormData(event.currentTarget);
+      if (
+        formData.get('file') === '' ||
+        formData.get('name') === '' ||
+        formData.get('email') === ''
+      ) {
+        alert('모든 값을 입력해주세요');
+        return;
+      }
+      const data = {
+        name: formData.get('name'),
+        email: formData.get('email'),
+      };
+      const fileData = formData.get('file');
+      const storageRef = await uploadImage(fileData, data);
+      const photoUrl = await getDownloadURL(storageRef);
+      data.photoUrl = photoUrl;
+      await uploadData(data);
+      window.history.pushState(null, null, '/');
+      routeRender();
+    }; // submit을 누르면  모두 입력이 되었는 지 확인하는 함수
+
+    form.addEventListener('submit', handleSubmit);
+  }
+}
