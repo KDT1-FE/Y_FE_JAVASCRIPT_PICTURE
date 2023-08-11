724f829032ef2005257aff7986eb889e29ae0e45
turkey-kim <rlaxmrgml@naver.com>
2023-08-11 18:36:25 +0900

    feat: DOM객체 상수화 및 스타일 수정 2023.08.11 1. 자주 쓰는 DOM객체 상수화 2. 프로필 등록 폼 스타일 수정

---
+++ constant.js
@@ -0,0 +1,11 @@
+export const btn_openEnrollForm = document.getElementById("open-enroll-form");
+
+// 직원등록폼 DOM객체
+export const photoInput = document.getElementById("photo-input");
+export const enrollForm = document.getElementsByClassName("enroll-form")[0];
+export const btn_addProfile = document.getElementsByClassName(
+  "enroll-form__submit"
+)[0];
+export const btn_closeEnrollForm = document.getElementsByClassName(
+  "enroll-form__cancel"
+)[0];
--- function/addProfile.js
+++ function/addProfile.js
@@ -1,24 +1,26 @@
-const enrollBtn = document.getElementById("enroll-profile-btn");
-const form = document.getElementsByClassName("add-profile")[0];
-const image = document.getElementById("photo-input");
-const submit = document.getElementsByClassName("add-profile__submit")[0];
-const cancel = document.getElementsByClassName("add-profile__cancel")[0];
+import {
+  btn_openEnrollForm,
+  photoInput,
+  enrollForm,
+  btn_addProfile,
+  btn_closeEnrollForm,
+} from "/constant.js";
 
-enrollBtn.addEventListener("click", openForm); // 프로필 등록 폼 열기
-cancel.addEventListener("click", closeForm); // 프로필 등록 폼 닫기
-image.addEventListener("change", previewImg); // 이미지 프리뷰
+btn_openEnrollForm.addEventListener("click", openForm); // 프로필 등록 폼 열기
+btn_closeEnrollForm.addEventListener("click", closeForm); // 프로필 등록 폼 닫기
+photoInput.addEventListener("change", previewImg); // 이미지 프리뷰
 
 function previewImg() {
-  const imageSrc = URL.createObjectURL(image.files[0]);
+  const imageSrc = URL.createObjectURL(photoInput.files[0]);
   document.getElementById("preview").src = imageSrc;
 }
 
 function openForm(e) {
   e.preventDefault();
-  form.style.display = "flex";
+  enrollForm.style.display = "flex";
 }
 
 function closeForm(e) {
   e.preventDefault();
-  form.style.display = "none";
+  enrollForm.style.display = "none";
 }
