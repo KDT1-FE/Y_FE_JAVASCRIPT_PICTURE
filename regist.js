0e434f5f140cb020d271971f3be6788b95d64429
jisu Seo <jseo9732@gmail.com>
2023-08-11 16:05:41 +0900

    Feat: 고객 등록 기능 구현

--- regist.js
+++ regist.js
@@ -1,20 +1,58 @@
-// import { ref, uploadBytes } from "firebase/storage";
-// import { storage } from "./firebase";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "./firebase";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { storage } from "./firebase";
 
-// const imageInputEl = document.getElementById("profile_pic");
+const imageInputEl = document.getElementById("profile_pic");
+const registForm = document.querySelector(".regist-form");
 
-// imageInputEl.addEventListener("change", (event) => {
-//   const file = document.getElementById("profile_pic").files[0];
-//   const storageRef = ref(storage, "images/mountains.jpg");
-//   uploadBytes(storageRef, file).then((snapshot) => {
-//     console.log("Uploaded a blob or file!");
-//   });
-// });
+// 프로필 이미지가 바뀌면 파이어베이스 Storage에 저장하고 화면에 표시
+imageInputEl.addEventListener("change", (event) => {
+  const file = document.getElementById("profile_pic").files[0];
+  const storageRef = ref(storage, "avatar/" + file.name);
+  uploadBytes(storageRef, file).then((snapshot) => {
+    getDownloadURL(storageRef).then((url) => {
+      // 프로필 이미지 url input에 저장
+      registForm.elements[1].value = url;
+      // 프로필 이미지가 있으면 <img> 삭제
+      const avartarImg = document.querySelector(".avatar");
+      if (avartarImg) {
+        avartarImg.remove();
+      }
+      // 프로필 이미지 추가
+      const avartarBox = document.getElementById("avatarBox");
+      const imgTag = document.createElement("img");
+      imgTag.src = url;
+      imgTag.className = "avatar";
+      avartarBox.appendChild(imgTag);
+    });
+  });
+});
 
-// const imageUploadFunc = () => {
-//   //   console.log(file.name);
-//   uploadBytes(storageRef, file).then((snapshot) => {
-//     console.log("Uploaded a blob or file!");
-//   });
-// };
-// var uploadTask = storageRef.child("images/" + file.name).put(file, metadata);
+// 완료 버튼 누르면 firestore에 회원 정보 저장
+registForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  try {
+    const docRef = await addDoc(collection(db, "customers"), {
+      avatar: registForm.elements[1].value,
+      name: registForm.elements[2].value,
+      email: registForm.elements[3].value,
+      phone: registForm.elements[4].value,
+      grade: registForm.elements[5].value,
+    });
+    console.log("Document written with ID: ", docRef.id);
+    location.href = "/";
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+});
+
+// input 태그에서 엔터 눌러도 submit 막기
+const textInput = document.querySelectorAll(".regist-text-input");
+for (const i of textInput) {
+  i.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+    }
+  });
+}
