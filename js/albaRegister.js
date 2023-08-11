2b45fc18cae94d6ba3a8a56df23ba7f6f0374b3b
junkue20 <junkue17@naver.com>
2023-08-11 18:06:52 +0900

    Update: 코드 정렬 및 개행문자 추가

--- js/albaRegister.js
+++ js/albaRegister.js
@@ -1,22 +1,24 @@
-// 이미지 등록 시 
-      function handleImageChange(event) {
-        const imagePreview = document.getElementById("imagePreview");
-        const selectedImage = event.target.files[0];
+// 이미지 등록 시
+function handleImageChange(event) {
+  const imagePreview = document.getElementById("imagePreview");
+  const selectedImage = event.target.files[0];
 
-        if (selectedImage) { // 이미지 등록 시 
-          const reader = new FileReader();
-          reader.onload = function (e) {
-            imagePreview.setAttribute("src", e.target.result); // 등록한 파일로 이미지변경
-          };
-          reader.readAsDataURL(selectedImage);
-        } else { // 이미지 등록이 안된 상태는 기본 이미지 출력
-          imagePreview.setAttribute("src", "../asset/pictures/no-image.png");
-        }
-      }
+  if (selectedImage) {
+    // 이미지 등록 시
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      imagePreview.setAttribute("src", e.target.result); // 등록한 파일로 이미지변경
+    };
+    reader.readAsDataURL(selectedImage);
+  } else {
+    // 이미지 등록이 안된 상태는 기본 이미지 출력
+    imagePreview.setAttribute("src", "../asset/pictures/no-image.png");
+  }
+}
 
-      // '이전으로' 버튼 기능
-      const backButton = document.querySelector(".back-button");
-      backButton.addEventListener("click", () => {
-        // window.history.back(); // 브라우저의 이전 페이지로 이동
-        window.location.href = "./myAlba.html";
-      });
+// '이전으로' 버튼 기능
+const backButton = document.querySelector(".back-button");
+backButton.addEventListener("click", () => {
+  // window.history.back(); // 브라우저의 이전 페이지로 이동
+  window.location.href = "myAlba.html";
+});
--- js/loading.js
+++ js/loading.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     const loadingScreen = document.querySelector(".loading-screen");
-
+    
     // 로딩이 완료되면 로딩 화면을 천천히 사라지게 함
     setTimeout(() => {
       loadingScreen.style.opacity = "0";
@@ -9,3 +9,4 @@ window.onload = function () {
       }, 500); // 로딩 화면이 완전히 사라지도록 딜레이 설정
     }, 1000); // 로딩 화면이 보이는 시간
   };
+  
