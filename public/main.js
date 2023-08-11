6113e2a8fd2d51f6721e727529d95f9b5185e8c6
developer-jyyun <developer.jyyun@gmail.com>
2023-08-11 19:31:25 +0900

    fix: Modify detail path(href)

--- public/main.js
+++ public/main.js
@@ -8,7 +8,7 @@
                             <dt class="name"> ${doc.data().이름}</dt>
                             <dd> ${doc.data().소속팀} ${doc.data().직급}</dd>
                             <dd> ${doc.data().내선번호}</dd>   
-                            <dd><a class="more" href="/detail.html?id=${doc.id}">상세 <i class="fa-solid fa-circle-plus"></i></a><p/>       
+                            <dd><a class="more" href="/detail/detail.html?id=${doc.id}">상세 <i class="fa-solid fa-circle-plus"></i></a><p/>       
                         </dl>`
       const wrap = document.querySelector('.container')
       wrap.insertAdjacentHTML('beforeend',template)
