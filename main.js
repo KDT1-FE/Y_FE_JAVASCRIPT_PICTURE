a3007ffd0b4c50f73d96d1eb36676efb5357234e
jinjoo-jung <wjdwlswn23@naver.com>
2023-08-11 15:46:21 +0900

    Update: main.gs

--- main.js
+++ main.js
@@ -1,23 +1,43 @@
 const list  = document.getElementById('list');
 const btn1 = document.getElementById('nav-btn1');
+const profileImageInput = document.getElementById('img');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const categoryInput = document.getElementById('category');
 
+btn1.addEventListener('click', () => {
+    const profileImage = profileImageInput.files[0];
+    const name = nameInput.value;
+    const email = emailInput.value;
+    const category = categoryInput.value;
 
-let staffs = [];
+    if (profileImage && name && email && category) {
+        const employeeDiv = document.createElement('div');
+        employeeDiv.classList.add('employee');
 
-btn1.addEventListener('click',createNewStaff);
+        const profileImageElement = document.createElement('img');
+        profileImageElement.src = URL.createObjectURL(profileImage);
+        profileImageElement.alt = name;
+        profileImageElement.width = 50;
 
-function createNewStaff () {
-     const item = {
-        id: new DataTransfer().getTime(),
-        text: "",
-        complete:false
+        const infoDiv = document.createElement('div');
+        infoDiv.innerHTML = `
+            <strong>${name}</strong>
+            <p>${email}</p>
+            <p>${category}<p>
+        `;
 
-     }
+        employeeDiv.appendChild(profileImageElement);
+        employeeDiv.appendChild(infoDiv);
 
-     createStaffElemnt();
-   
-}
-function createStaffElemnt(item) {
-    const itemEl = document.createElement('div')
-    
-}
+        employeeList.appendChild(employeeDiv);
+
+        // Clear input fields
+        profileImageInput.value = '';
+        nameInput.value = '';
+        emailInput.value = '';
+        categoryInput.value='';
+    } else {
+        alert('모든 필드를 입력하세요.');
+    }
+});
