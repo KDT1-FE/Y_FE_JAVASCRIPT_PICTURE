4e14055c6db6eb5d24b5295d3c91ac2865b28770
JitHoon <imessi0336@gmail.com>
2023-08-11 12:36:59 +0900

    Env: Setting webpack and Install sass for use scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,29 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+  // 번들링 위치, scss의 경우 index.js 파일에 import
   entry: {
     index: "./src/js/index.js",
     fb: "./src/js/fb.js"
   },
   mode: "development",
+  // .js.map 파일 생성
+  devtool: "source-map",
+  // 번들링 js 파일이 저장될 경로와 이름 지정
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, "assets", "js"),
     clean: true
   },
   watch: true,
+  plugins: [
+    // 컴파일 + 번들링 CSS 파일이 저장될 경로와 이름 지정
+    new MiniCssExtractPlugin({ filename: "../css/style.css" })
+  ],
   module: {
     rules: [
+      // js 번들링
       {
         test: /\.js$/,
         use: {
@@ -23,6 +33,12 @@ module.exports = {
             compact: false
           }
         }
+      },
+      // scss 컴파일 및 번들링
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        exclude: /node_modules/
       }
     ]
   },
