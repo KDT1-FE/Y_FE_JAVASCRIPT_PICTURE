bd5348dabaebc4eb091737a7a15637e2a9d87039
JitHoon <imessi0336@gmail.com>
2023-08-11 11:55:06 +0900

    Env: Setting webpack and babel for js

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    index: "./src/js/index.js"
+  },
+  mode: "development",
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "assets", "js"),
+    clean: true
+  },
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+            compact: false
+          }
+        }
+      }
+    ]
+  },
+  experiments: {
+    topLevelAwait: true
+  }
+};
