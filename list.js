a402a4ad3183baa3f68531412d4e14689dec037f
jisu Seo <jseo9732@gmail.com>
2023-08-11 18:38:24 +0900

    Feat: 고객 삭제 기능 구현

--- list.js
+++ list.js
@@ -1,4 +1,4 @@
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, doc, deleteDoc } from "firebase/firestore";
 import { db } from "./firebase";
 
 // 리스트 뿌려주기
@@ -11,6 +11,8 @@ window.onload = async () => {
 
     const checkTag = document.createElement("input");
     checkTag.type = "checkbox";
+    checkTag.className = "delete-checkbox";
+    checkTag.value = doc.id;
 
     const avatarBox = document.createElement("div");
     avatarBox.className = "avatar-box";
@@ -44,5 +46,32 @@ window.onload = async () => {
 
     const listContainer = document.querySelector(".list-container");
     listContainer.appendChild(BoxTag);
+  });
+
+  // 고객 삭제 기능
+  let deleteList = [];
+  // 체크한 고객 배열에 삽입
+  const deleteChek = document.querySelectorAll(".delete-checkbox");
+  for (const i of deleteChek) {
+    i.addEventListener("click", (e) => {
+      if (i.checked) {
+        deleteList.push(i.value);
+      } else {
+        deleteList = deleteList.filter((e) => e !== i.value);
+      }
+    });
+  }
+  // 버튼 클릭하면 삭제 요청
+  const deleteBtn = document.getElementById("deleteBtn");
+  deleteBtn.addEventListener("click", async (e) => {
+    if (deleteList.length > 0) {
+      deleteList.forEach(async (id) => {
+        await deleteDoc(doc(db, "customers", id)).then(() => {
+          location.reload();
+        });
+      });
+    } else {
+      alert("삭제할 고객을 선택하세요.");
+    }
   });
 };
