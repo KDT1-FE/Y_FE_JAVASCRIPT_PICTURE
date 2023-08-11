c2fd540d77c7998fade8cbbee647bb8f0fa27b55
gaori <monooi@naver.com>
2023-08-11 15:00:00 +0900

    Feat: 무한 스크롤 기능 추가

--- src/displayUserList.js
+++ src/displayUserList.js
@@ -1,40 +1,79 @@
-import { collection, onSnapshot, query, orderBy } from "firebase/firestore";
+import {
+  collection,
+  query,
+  orderBy,
+  limit,
+  getDocs,
+  startAfter,
+} from "firebase/firestore";
 import { db } from "./firebase";
 
 const userListContainer = document.querySelector(".user__list");
+const scrollEnd = document.querySelector(".scroll-end");
 
 // 이름순 정렬
 const userRef = collection(db, "users");
-const nameOrderQuery = query(userRef, orderBy("name"));
 
-const unsub = onSnapshot(nameOrderQuery, (snapshot) => {
-  userListContainer.innerHTML = "";
+// 무한 스크롤 및 로딩 대기
+const SIZE = 8;
+let lastVisible;
+async function fetchUsers(afterDoc) {
+  const options = [userRef, orderBy("name"), limit(SIZE)];
 
-  snapshot.forEach((doc) => {
-    console.log(doc.data(), doc.id);
+  if (afterDoc) {
+    options.push(startAfter(afterDoc));
+  }
+
+  const userQuery = query(...options);
+  const snapshots = await getDocs(userQuery);
+
+  if (snapshots.size < SIZE) {
+    observer.unobserve(scrollEnd);
+  }
+
+  snapshots.forEach((doc) => {
+    const userDiv = document.createElement("div");
+    userDiv.className = "user__user";
+    userDiv.dataset.id = doc.id;
     const user = doc.data();
     const innerHTML = `
-      <div class="user__user" data-id=${doc.id}>
-        <img src=${user.imageUrl} alt="profile image" class="user__image" />
-        <div class="user__info">
-          <h3 class="user__name">${user.name}</h3>
-          <p class="user__email">${user.email}</p>
-          <p class="user__position">${user.position}</p>
+      <img src=${user.imageUrl} alt="profile image" class="user__image" />
+      <div class="user__info">
+        <h3 class="user__name">${user.name}</h3>
+        <p class="user__email">${user.email}</p>
+        <p class="user__position">${user.position}</p>
+      </div>
+      <div class="user__menu-icon">
+        <i class="fa-solid fa-ellipsis-vertical"></i>
+      </div>
+      <div class="user__menu-items flex-center hidden">
+        <div class="user__menu-edit flex-center pointer">
+          <i class="fa-solid fa-pen"></i>
         </div>
-        <div class="user__menu-icon">
-          <i class="fa-solid fa-ellipsis-vertical"></i>
-        </div>
-        <div class="user__menu-items flex-center hidden">
-          <div class="user__menu-edit flex-center pointer">
-            <i class="fa-solid fa-pen"></i>
-          </div>
-          <div class="user__menu-delete flex-center pointer">
-            <i class="fa-solid fa-eraser"></i>
-          </div>
+        <div class="user__menu-delete flex-center pointer">
+          <i class="fa-solid fa-eraser"></i>
         </div>
       </div>
     `;
+    userDiv.innerHTML = innerHTML;
+    userListContainer.appendChild(userDiv);
+  });
 
-    userListContainer.insertAdjacentHTML("beforeend", innerHTML);
+  lastVisible = snapshots.docs[snapshots.docs.length - 1];
+}
+
+const observer = new IntersectionObserver(async (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      observer.unobserve(entry.target);
+      fetchUsers(lastVisible);
+    }
   });
 });
+
+async function initialize() {
+  await fetchUsers();
+  observer.observe(scrollEnd);
+}
+
+initialize();
