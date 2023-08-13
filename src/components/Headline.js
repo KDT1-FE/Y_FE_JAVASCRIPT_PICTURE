da9aff1cedfe9c8d633805bbe28f3c1edda8399c
TaePoong719 <just5538@gmail.com>
2023-08-13 17:13:34 +0900

    Design: 글꼴 추가, Headline 디자인 변경, 색깔 변수 지정

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -4,8 +4,9 @@ export default class Headline extends Component{
   render(){
     this.el.classList = "headline"
     this.el.innerHTML = /* html */`
-      <h1> League of Legends</h1>
-      <h2> 당신이 알고싶은 챔피언을 검색하시오 <h2>
+      
+      <img src=https://firebasestorage.googleapis.com/v0/b/employee-management-c0a21.appspot.com/o/icons%2Flogo-1200-04b3cefafba917c9c571f9244fd28a1e%20(1).png?alt=media&token=6960ba53-cf19-4fed-844a-8fd0aad3696a > 
+      <h2> 당신이 알고싶은 <span>챔피언을</span> 검색하시오 </h2>
     `
   }
 }
