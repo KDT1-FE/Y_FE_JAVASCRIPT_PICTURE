bc46956fdd4e99c025b0b6b0ac6d5996843345eb
TaePoong719 <just5538@gmail.com>
2023-08-13 16:37:36 +0900

    Design: 챔피언 호버 애니메이션 추가

--- src/components/ChampionItem.js
+++ src/components/ChampionItem.js
@@ -5,7 +5,6 @@ import championStore from "../store/champion";
 export default class ChampionItem extends Component{
   constructor(props){
     super({
-      tagName:'a',
       props
     })
     championStore.subscribe('isDeleteState', ()=>{
@@ -13,20 +12,23 @@ export default class ChampionItem extends Component{
   }
   render(){
     const champion = this.props
-    this.el.setAttribute("href",`#/champion?name=${champion.name}`)
-    this.el.classList.add('champion')
-    this.el.style.backgroundImage = `url(${champion.thumbnail})`
+    this.el.classList.add('champion-wrap')
     this.el.innerHTML = /* html */`
-      <div class="iconwrap">
-      </div>
-      <div class="info">
-        <h2>${champion.nickname}</h2>
-        <h1>${champion.name}</h1>
-      </div>
-
-      <div class="champion-item-delete"><input type="checkbox" class="hide input-delete" value="delete" id="delete${champion.name}"></div>
-      <label for="delete${champion.name}" style=" position:absolute; top:0; left:0; bottom:0; right:0; backgroundColor:yellow "></label>
+      <a class="champion">
+        <div class="iconwrap">
+        </div>
+        <div class="info">
+          <h2>${champion.nickname}</h2>
+          <h1>${champion.name}</h1>
+        </div>
+          <div class="champion-item-delete"><input type="checkbox" class="hide input-delete" value="delete" id="delete${champion.name}"></div>
+          <label for="delete${champion.name}" style=" position:absolute; top:0; left:0; bottom:0; right:0; backgroundColor:yellow "></label>
+      </a>
     `
+    const championEl = this.el.querySelector('a')
+    championEl.setAttribute("href",`#/champion?name=${champion.name}`)
+    championEl.style.backgroundImage = `url(${champion.thumbnail})`
+    
     const iconwrapEl = this.el.querySelector('.iconwrap')
     const deleteInputEl = this.el.querySelector('.input-delete')
 
@@ -38,14 +40,13 @@ export default class ChampionItem extends Component{
     
     if(championStore.state.isDeleteState){
       deleteInputEl.classList.remove('hide')
-      this.el.style.backgroundBlendMode = "darken"
+      championEl.style.backgroundBlendMode = "darken"
     }else{
       deleteInputEl.classList.add('hide')
-      this.el.style.backgroundBlendMode = "normal"
+      championEl.style.backgroundBlendMode = "normal"
     }
     // championStore.state.deleteObj = {}
     deleteInputEl.addEventListener('input',event=>{
-      console.log(event)
       championStore.state.deleteObj[champion.name] = event.target.checked
     })
   }
