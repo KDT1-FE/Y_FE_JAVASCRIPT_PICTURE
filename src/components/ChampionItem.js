14484a57af5701ff7e97e8a8455a165f6511087c
TaePoong719 <just5538@gmail.com>
2023-08-13 17:16:33 +0900

    Fix: a태그 클릭 안되는 버그 수정

--- src/components/ChampionItem.js
+++ src/components/ChampionItem.js
@@ -22,7 +22,7 @@ export default class ChampionItem extends Component{
           <h1>${champion.name}</h1>
         </div>
           <div class="champion-item-delete"><input type="checkbox" class="hide input-delete" value="delete" id="delete${champion.name}"></div>
-          <label for="delete${champion.name}" style=" position:absolute; top:0; left:0; bottom:0; right:0; backgroundColor:yellow "></label>
+          <label class="hide" for="delete${champion.name}" style=" position:absolute; top:0; left:0; bottom:0; right:0; backgroundColor:yellow "></label>
       </a>
     `
     const championEl = this.el.querySelector('a')
@@ -31,6 +31,7 @@ export default class ChampionItem extends Component{
     
     const iconwrapEl = this.el.querySelector('.iconwrap')
     const deleteInputEl = this.el.querySelector('.input-delete')
+    const labelEl = this.el.querySelector('label')
 
     iconwrapEl.append(
       new Icon(champion.region).el,
@@ -39,10 +40,12 @@ export default class ChampionItem extends Component{
     )
     
     if(championStore.state.isDeleteState){
+      labelEl.classList.remove('hide')
       deleteInputEl.classList.remove('hide')
       championEl.style.backgroundBlendMode = "darken"
     }else{
       deleteInputEl.classList.add('hide')
+      labelEl.classList.add('hide')
       championEl.style.backgroundBlendMode = "normal"
     }
     // championStore.state.deleteObj = {}
