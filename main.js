8f86c2816d7b8884d71c2d60ab74c6dd8d8ad69a
jinjoo-jung <wjdwlswn23@naver.com>
2023-08-14 12:03:11 +0900

    import export 추가

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import {AccessKeyId} from './aws_functions.js'
+import {SecretAccessKey} from './aws_functions.js'
+
 const list  = document.getElementById('list');
 const btn1 = document.getElementById('nav-btn1');
 const btn2 = document.getElementById('nav-btn2')
@@ -59,7 +62,6 @@ btn1.addEventListener('click', async () => {
             // 이미지를 AWS S3에 업로드하기 위해 putFile 함수 호출
             const result = await putFile(profileImage);
             console.log('Successfully uploaded photo:', result);
-            result.key
             //이미지 업로드 성공한 경우, 해당 이미지의 URL을 이용해 화면에 표시하거나 다른 작업 수행
             // const imageUrl = result.Location;
 
@@ -91,3 +93,47 @@ btn2.addEventListener('click', () => {
 
     })
 })
+
+
+// putFile 함수를 정의하고 업로드할 이미지 파일을 매개변수 file로 받는다.
+const putFile = file => {
+    const albumBucketName = 'js-employee-bucket'; // S3의 버킷 이름
+    const region = 'ap-northeast-2'; // 서울
+    const accessKeyId =  AccessKeyId;
+    const secretAccessKey = SecretAccessKey;
+
+  
+    AWS.config.update({
+      region,
+      accessKeyId,
+      secretAccessKey
+    }); 
+    
+    // 업로드를 관리하는 'upload' 객체를 생성하고
+    // 업로드할 이미지 파일의 정보와 업로드 옵션을 설정한다. 
+    const upload = new AWS.S3.ManagedUpload({
+      params: {
+        Bucket: albumBucketName,
+        Key: file.name,
+        Body: file,
+        ACL: "public-read"
+      }
+    });
+
+    
+    // 'upload' 객체의 업로드 작업을 수행하는 프로미스 생성
+    const promise = upload.promise();
+  
+
+    // 업로드 작업의 프로미스가 성공하면 첫 번째 콜백함수가 호출되어 콘솔에 출력
+    // 작업이 실패하면 두 번째 콜백 함수가 호출되어 에러메시지와 함께 에러 내용 콘솔 출력
+    promise.then(
+      function(data) {
+        console.log("Successfully uploaded photo: ", data);
+      },
+      function(err) {
+        return console.log("There was an error uploading your photo: ", err.message);
+      }
+    );
+  };
+
