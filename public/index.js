2e8affea94bf379b3216a13c5bbbd64892c3181f
박혜민 <hyeminpak@gmail.com>
2023-08-14 18:21:34 +0900

    feat: 이미지 첨부시 미리보기 기능구현

--- public/index.js
+++ public/index.js
@@ -2,6 +2,9 @@ const userAddBtnsEl = document.querySelectorAll("#user-add-btn");
 const cancelBtnEl = document.querySelector(".section__user_cancel_btn");
 const addBtnEl = document.querySelector(".section__user_add_btn");
 
+const imgEl = document.querySelector(".img_section");
+const img_inputEl = document.querySelector(".user_imgInput");
+
 userAddBtnsEl.forEach((btn) => {
   btn.addEventListener("click", () => {
     const userAddForm = document.querySelector(".section__user-add-box");
@@ -17,4 +20,21 @@ window.addEventListener("click", (event) => {
   if (event.target === userAddForm) {
     userAddForm.style.display = "none";
   }
+});
+
+function readURL(input) {
+  if (input.target.files && input.target.files[0]) {
+    let reader = new FileReader();
+
+    reader.onload = (e) => {
+      imgEl.setAttribute("src", e.target.result);
+    };
+    reader.readAsDataURL(input.target.files[0]);
+  }
+}
+img_inputEl.addEventListener("change", (e) => {
+  readURL(e);
+});
+imgEl.addEventListener("click", () => {
+  img_inputEl.click();
 });
