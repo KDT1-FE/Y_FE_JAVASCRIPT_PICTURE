4a44b923c9bea0e55dd704becbb7efe6461e7404
jiohjung98 <ckaquz98@naver.com>
2023-08-14 18:44:16 +0900

    Create: InfoDialog

--- script.js
+++ script.js
@@ -1,20 +1,66 @@
-// // script.js
-// import { auth, googleAuthProvider } from "./firebase.js";
-// import { signInWithPopup } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js"; // 추가
+// script.js
 
-// const registerButton = document.querySelector(".register-btn");
+// ...Firebase 초기화 코드 등...
 
-// registerButton.addEventListener("click", () => {
-//   // 구글 로그인 팝업 띄우기
-//   signInWithGooglePopup();
-// });
+const registerDogButton = document.getElementById("registerDogButton");
+const modalContainer = document.getElementById("addDogDialog");
+const closeModalButton = document.getElementById("closeModalButton");
+const dialogRegisterDogButton = document.getElementById("dialogRegisterDogButton");
+// 파일 입력 요소와 미리보기를 위한 이미지 요소 가져오기
+const dogImageInput = document.getElementById("dogImageInput");
+const imagePreview = document.getElementById("imagePreview");
 
-// // 구글 로그인 팝업을 띄우는 함수
-// async function signInWithGooglePopup() {
-//   try {
-//     await signInWithPopup(auth, googleAuthProvider); // signInWithPopup 함수를 사용
-//     console.log("구글 로그인 성공!");
-//     // 여기서 강아지 등록 페이지로 이동하는 로직을 추가할 수 있습니다.
-//   } catch (error) {
-//   }
-// }
+registerDogButton.addEventListener("click", () => {
+    modalContainer.style.display = "block"; // 모달 보이기
+    imagePreviewText.style.display = "block";
+    imagePreview.src = "#";
+});
+
+closeModalButton.addEventListener("click", () => {
+    // 이미지 미리보기 초기화 & '이미지 선택' 텍스트 보이게 
+    imagePreview.src = "#";
+    imagePreviewText.style.display = "block";
+    modalContainer.style.display = "none"; // 모달 숨기기
+});
+
+dialogRegisterDogButton.addEventListener("click", () => {
+    // 등록 처리하는 코드 추가
+
+    modalContainer.style.display = "none"; // 모달 숨기기
+});
+
+window.addEventListener("click", (event) => {
+    if (event.target === modalContainer) {
+        modalContainer.style.display = "none"; // 모달 숨기기
+    }
+});
+
+window.addEventListener("load", () => {
+    // displayDogInfo(); 여기서 강아지 정보 표시 함수 호출 등을 추가하세요.
+});
+
+// 파일 선택 시 이미지 미리보기
+const imagePreviewLabel = document.querySelector(".image-preview-circle span");
+
+// 파일 선택 시 이미지 미리보기
+dogImageInput.addEventListener("change", (event) => {
+  const selectedImage = event.target.files[0];
+
+  if (selectedImage) {
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      imagePreview.src = e.target.result;
+      imagePreview.style.display = "block";
+      imagePreviewLabel.style.display = "none"; // 선택된 파일 없음 텍스트 숨기기
+    };
+
+    reader.readAsDataURL(selectedImage);
+  }
+});
+
+// 이미지 미리보기 클릭 시 파일 선택 input 열기
+imagePreviewLabel.addEventListener("click", () => {
+  dogImageInput.click();
+});
+
