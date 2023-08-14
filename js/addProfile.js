33484f310cda66b052184111f4757c2ff9274418
turkey-kim <rlaxmrgml@naver.com>
2023-08-14 15:44:28 +0900

    feat: 사진 및 프로필 정보 업로드 기능 추가 2023.08.14 1. S3에 프로필 당 하나의 폴더 생성 후 해당 폴더에 사진 및 추가 정보 저장 2. 파일 분리 및 리펙토링 예정

--- js/addProfile.js
+++ js/addProfile.js
@@ -1,17 +1,40 @@
 import {
+  bucketName,
+  region,
+  accessKeyId,
+  secretAccessKey,
+  S3endpoint,
+} from "/js/config/aws.js";
+
+import {
   btn_openEnrollForm,
-  photoInput,
   enrollForm,
   btn_addProfile,
   btn_closeEnrollForm,
+  enroll_photo,
+  enroll_name,
+  enroll_email,
+  enroll_phoneNum,
+  enroll_position,
 } from "/js/constant.js";
+
+AWS.config.update({
+  region,
+  credentials: new AWS.Credentials(accessKeyId, secretAccessKey),
+});
+
+const s3 = new AWS.S3({
+  apiVersion: "2006-03-01",
+  params: { Bucket: bucketName },
+});
 
 btn_openEnrollForm.addEventListener("click", openForm); // 프로필 등록 폼 열기
 btn_closeEnrollForm.addEventListener("click", closeForm); // 프로필 등록 폼 닫기
-photoInput.addEventListener("change", previewImg); // 이미지 프리뷰
+enroll_photo.addEventListener("change", previewImg); // 이미지 프리뷰
+btn_addProfile.addEventListener("click", uploadProfileToS3); // s3로 프로필업로드
 
 function previewImg() {
-  const imageSrc = URL.createObjectURL(photoInput.files[0]);
+  const imageSrc = URL.createObjectURL(enroll_photo.files[0]);
   document.getElementById("preview").src = imageSrc;
 }
 
@@ -23,4 +46,43 @@ function openForm(e) {
 function closeForm(e) {
   e.preventDefault();
   enrollForm.style.display = "none";
+}
+
+function uploadProfileToS3(event) {
+  event.preventDefault();
+  const photo = enroll_photo.files[0]; // 업로드할 프로필 사진
+  const profileInfo = {
+    name: enroll_name.value,
+    email: enroll_email.value,
+    phoneNum: enroll_phoneNum.value,
+    position: enroll_position.value,
+  }; // 사진을 제외한 프로필 정보
+
+  const folerPath = `${profileInfo.name}/`; // 사진과 정보가 들어갈 S3폴더명(일단 이름으로)
+
+  const params_photo = {
+    Bucket: bucketName,
+    Key: folerPath + "profile_photo",
+    Body: photo,
+  };
+
+  const params_info = {
+    Bucket: bucketName,
+    Key: folerPath + "profile_detail",
+    Body: JSON.stringify(profileInfo),
+  };
+
+  const upload = async () => {
+    try {
+      await s3.upload(params_photo).promise();
+      console.log("photo uploaded");
+      await s3.upload(params_info).promise();
+      console.log("info uploaded");
+      location.reload();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  upload();
 }
--- js/constant.js
+++ js/constant.js
@@ -1,8 +1,14 @@
 export const btn_openEnrollForm = document.getElementById("open-enroll-form");
 
 // 직원등록폼 DOM객체
-export const photoInput = document.getElementById("photo-input");
 export const enrollForm = document.getElementsByClassName("enroll-form")[0];
+// 직원등록폼 상세기입 내용
+export const enroll_photo = document.getElementById("photo-input");
+export const enroll_name = document.getElementById("name-input");
+export const enroll_email = document.getElementById("email-input");
+export const enroll_phoneNum = document.getElementById("phoneNum-input");
+export const enroll_position = document.getElementById("position-input");
+// 직원등록폼 추가/취소 버튼
 export const btn_addProfile = document.getElementsByClassName(
   "enroll-form__submit"
 )[0];
