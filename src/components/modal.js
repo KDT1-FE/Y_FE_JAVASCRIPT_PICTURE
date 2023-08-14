ce8d152e29c3fe13673668b2c533c05999479c3f
HOOOO98 <loading2us@gmail.com>
2023-08-14 14:25:18 +0900

    Add: history.html, roles.html

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,1 +1,38 @@
+// Modal open
+const openModal = () => {
+    document.querySelector(".modal").classList.remove("hidden");
+}
 
+const closeModal = () => {
+    document.querySelector(".modal").classList.add("hidden");
+}
+
+document.querySelector(".imprisonBtn").addEventListener("click", openModal);
+document.querySelector(".dismissBtn").addEventListener("click", closeModal);
+document.querySelector(".modalBg").addEventListener("click", closeModal);
+
+// form submit
+const infoForm = document.getElementById('infoForm');
+const responseBox = document.getElementById('responseBox');
+
+infoForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const prisonerName = document.getElementById('prisonerName').value;
+    const prisonerPhoto = document.getElementById('prisonerPhoto').files[0];
+    // const prisonerLv = document.getElementById('prisonerLv').value;
+
+    const reader = new FileReader();
+    reader.onload = function (e) {
+    const responseBox = document.querySelector('.prison');
+    const newPrisonerCell = document.createElement('div');
+    newPrisonerCell.classList.add('prisonCell');
+    newPrisonerCell.style.backgroundImage = `url(${e.target.result})`;
+    newPrisonerCell.alt = `${prisonerName} Mugshot`;
+    newPrisonerCell.innerHTML = /*html*/`
+        <p class="prisonerName">${prisonerName}</p>
+    `;                            
+    responseBox.appendChild(newPrisonerCell);
+};
+    reader.readAsDataURL(prisonerPhoto);
+    closeModal();
+});
