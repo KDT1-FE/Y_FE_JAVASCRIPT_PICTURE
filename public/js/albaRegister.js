905b6cd5a041e215263592b1ce1e38d0b9b1418d
junkue20 <junkue13@gmail.com>
2023-08-15 11:49:19 +0900

    Feat: 이미지 및 인적사항 업로드 기능 추가

--- public/js/albaRegister.js
+++ public/js/albaRegister.js
@@ -16,11 +16,80 @@ function handleImageChange(event) {
   }
 }
 // ------------------------------------------------------------------------
+// Firebase SDK
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCUfBccVzvG5Eh25qNxqpuerxn2NXotpxo',
+  authDomain: 'we-alba-b3313.firebaseapp.com',
+  projectId: 'we-alba-b3313',
+  storageBucket: 'we-alba-b3313.appspot.com',
+  messagingSenderId: '495601569025',
+  appId: '1:495601569025:web:a2ad855c6c90f52eed9f00',
+};
+
+// Firebase 초기화
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+$('#sendButton').click(function () {
+  // 이미지 업로드 기능
+  var file = document.querySelector('#photoInput').files[0];
+  var storageRef = storage.ref();
+  var 저장할경로 = storageRef.child('image/' + file.name);
+  var 업로드작업 = 저장할경로.put(file);
+
+  업로드작업.on(
+    'state_changed',
+    // 변화시 동작하는 함수
+    null,
+    //에러시 동작하는 함수
+    (error) => {
+      console.error('실패사유: ', error);
+    },
+    // 성공시 동작하는 함수
+    () => {
+      업로드작업.snapshot.ref.getDownloadURL().then((url) => {
+        console.log('업로드된 경로는', url);
+
+        // 인적사항 업로드 기능
+        var albaInfo = {
+          이름: $('#name').val(),
+          연락처: $('#phone').val(),
+          직급: $('#position').val(),
+          근무시간: $('#workingHours').val(),
+          등록날짜: new Date(),
+          이미지: url,
+        };
+
+        // 유효성 검사 코드
+        if (albaInfo.이름 === '' || albaInfo.연락처 === '' || albaInfo.직급 === '' || albaInfo.근무시간 === '') {
+          alert('빈 값이 있습니다.');
+          return;
+        }
+
+        db.collection('albainfo')
+          .add(albaInfo)
+          .then((result) => {
+            window.location.href = '/albaSelect.html';
+            console.log(result);
+          })
+          .catch((err) => {
+            alert('등록실패!');
+            console.log(err);
+          });
+      });
+    }
+  );
+});
 
 // ------------------------------------------------------------------------
 
 // '이전으로' 버튼 기능
 const backButton = document.querySelector(".back-button");
 backButton.addEventListener("click", () => {
-  window.location.href = "./myAlba.html";
+  window.history.go(-1);
 });
+
+// ------------------------------------------------------------------------
