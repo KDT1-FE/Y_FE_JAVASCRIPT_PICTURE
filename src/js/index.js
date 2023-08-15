fe436ecd8552374c2354cb9f7949f907468dfc25
JiHongkyu <hongkyu9941@gmail.com>
2023-08-15 18:10:22 +0900

    Update: 삭제하기 제작

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,30 @@
 const tBody = document.querySelector('tbody');
+const modal = document.querySelector('.modal');
+const profileBtn = document.querySelector('.profile-btn');
+const deleteBtn = document.querySelector('.delete-btn');
+const modalCancel = document.querySelector('.modal-cancel');
+let getItem = JSON.parse(localStorage.getItem('infos'));
 
-let getItem = localStorage.getItem('infos');
+// localStorage에서 정보들 가져와서 리스트 생성
 if (getItem) {
-  getItem = JSON.parse(getItem);
-
   getItem.forEach((item) => {
     createStaffList(item);
   });
 }
 
+// 리스트 생성 함수
 function createStaffList(item) {
   const tr = document.createElement('tr');
+  const phone1 = item['phone'].slice(0, 3);
+  const phone2 = item['phone'].slice(3, 7);
+  const phone3 = item['phone'].slice(7);
 
   tr.innerHTML = `
-    <td><input type="checkbox" /></td>
-    <td><img src="${item.imageUrl}" alt="사진이었던 것" /></td>
-    <td>${item.name}</td>
-    <td>${item.email}</td>
-    <td>${item.phone}</td>
-    <td>${item.address}</td>
+  <td><img src="${item.imageUrl}" alt="사진이었던 것" /></td>
+  <td>${item.name}</td>
+  <td>${item.email}</td>
+  <td>${phone1}-${phone2}-${phone3}</td>
+  <td>${item.address}</td>
   `;
 
   tBody.append(tr);
@@ -28,6 +34,27 @@ function createStaffList(item) {
     const data = JSON.stringify(item);
     localStorage.setItem('lately-info', data);
 
-    location.href = '/profile.html';
+    // 상세보기 or 삭제하기 버튼 클릭
+    modal.classList.add('active');
+    modalCancel.addEventListener('click', function () {
+      modal.classList.remove('active');
+    });
+    // 상세보기 버튼
+    profileBtn.addEventListener('click', function () {
+      modal.classList.remove('active');
+      location.href = '/profile.html';
+    });
+
+    // 삭제하기 버튼
+    deleteBtn.addEventListener('click', function () {
+      getItem = getItem.filter((el) => el['id'] !== item['id']);
+
+      // localStorage 리스트 다시 생성
+      const data = JSON.stringify(getItem);
+      localStorage.setItem('infos', data);
+      tr.remove();
+
+      modal.classList.remove('active');
+    });
   });
 }
--- src/js/profile.js
+++ src/js/profile.js
@@ -7,10 +7,13 @@ const staffInfoEdit = document.querySelector('.staff-info-edit');
 
 // 방금 생성한 것이거나 마지막으로 클릭한 임직원 정보
 const data = JSON.parse(localStorage.getItem('lately-info'));
+const phone1 = data['phone'].slice(0, 3);
+const phone2 = data['phone'].slice(3, 7);
+const phone3 = data['phone'].slice(7);
 
 profileName.innerText = data.name;
 profileEmail.innerText = data.email;
-profilePhone.innerText = data.phone;
+profilePhone.innerText = `${phone1}-${phone2}-${phone3}`;
 profileAddress.innerText = data.address;
 profileImage.src = data.imageUrl;
 
