34393c6e7186005faea3f10bc5e5c49e881be17d
JinJeongMin <jdsjm2@naver.com>
2023-08-16 01:41:42 +0900

    Feat: db.js 생성 및 firebase 연결

---
+++ main.js
@@ -0,0 +1,3 @@
+import { app } from "./src/utils/db.js";
+
+console.log(app);
---
+++ src/utils/db.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD9fbsD-hZaGNcRnVuoKJJ_nFagysrdm6E",
+  authDomain: "employeemanagement-9fd1f.firebaseapp.com",
+  databaseURL:
+    "https://employeemanagement-9fd1f-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "employeemanagement-9fd1f",
+  storageBucket: "employeemanagement-9fd1f.appspot.com",
+  messagingSenderId: "30056874245",
+  appId: "1:30056874245:web:6206f3abd5d7ef9864376c"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
