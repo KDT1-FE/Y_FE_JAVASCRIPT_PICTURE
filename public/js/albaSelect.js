40e5951622cb0500868e5c6df4623c9bc6a47893
junkue20 <junkue13@gmail.com>
2023-08-15 11:50:33 +0900

    Feat: data 받아오기, 삭제, 프로필조회 기능 추가

--- public/js/albaSelect.js
+++ public/js/albaSelect.js
@@ -1,11 +1,144 @@
 // '알바생 등록하기' 버튼 기능
-const registerButton = document.querySelector(".alba-register-button");
-backButton.addEventListener("click", () => {
-  window.location.href = "./albaRegister.html";
+const registerButton = document.querySelector('.alba-register-button');
+registerButton.addEventListener('click', () => {
+  window.location.href = './albaRegister.html';
 });
 
 // '이전 페이지로' 버튼 기능
-const backButton = document.querySelector(".back-button");
-backButton.addEventListener("click", () => {
-  window.location.href = "./myAlba.html";
+const backButton = document.querySelector('.back-button');
+backButton.addEventListener('click', () => {
+  window.location.href = './myAlba.html';
+});
+
+/* ----------------------------------------------------------------------------------- */
+
+// Firebase SDK
+const firebaseConfig = {
+  apiKey: 'AIzaSyCUfBccVzvG5Eh25qNxqpuerxn2NXotpxo',
+  authDomain: 'we-alba-b3313.firebaseapp.com',
+  projectId: 'we-alba-b3313',
+  storageBucket: 'we-alba-b3313.appspot.com',
+  messagingSenderId: '495601569025',
+  appId: '1:495601569025:web:a2ad855c6c90f52eed9f00',
+};
+
+// Firebase 초기화
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+// 알바생 데이터 받아옴
+db.collection('albainfo')
+  .get()
+  .then((result) => {
+    result.forEach((doc) => {
+      var tr = /* html */ `
+      <tr>
+        <td class="select-checkbox">
+          <label class="container">
+            <input type="checkbox" />
+            <div class="checkmark"></div>
+          </label>
+        </td>
+        <td class="alba-photo">
+          <img src="${doc.data().이미지}" alt="알바생 사진" />
+        </td>
+        <td class="alba-name"><p>${doc.data().이름}</p></td>
+        <td class="alba-phone"><p>${doc.data().연락처}</p></td>
+        <td class="alba-class"><p>${doc.data().직급}</p></td>
+        <td class="alba-worktime"><p>${doc.data().근무시간}</p></td>
+        <td><button class="edit-button">조회하기</button></td>
+      </tr>
+      `;
+      $('.alba-list-body').append(tr);
+    });
+  });
+
+// '조회하기' 버튼 클릭 이벤트 리스너 등록
+$(document).on('click', '.edit-button', function () {
+  // 클릭한 버튼의 부모 요소에서 데이터 가져오기
+  const $parentRow = $(this).closest('tr');
+  const phoneNum = $parentRow.find('.alba-phone p').text();
+
+  // 데이터베이스에서 해당하는 데이터 가져오기
+  db.collection('albainfo')
+    .where('연락처', '==', phoneNum)
+    .get()
+    .then((querySnapshot) => {
+      if (!querySnapshot.empty) {
+        const doc = querySnapshot.docs[0];
+        const data = doc.data();
+        // 모달창에 데이터 채우기
+        $('.modal-left-container img').attr('src', data.이미지);
+        $('.modal-right-container p:eq(0)').text(`${data.이름}`);
+        $('.modal-right-container p:eq(1)').text(`${data.직급}`);
+        $('.modal-right-container p:eq(2)').text(`${data.연락처}`);
+        $('.modal-right-container p:eq(3)').text(`${data.근무시간}`);
+        // 모달창 띄우기
+        $('.modal-container').fadeIn();
+
+        // 모달창 버튼 클릭 이벤트 리스너 등록
+        $('.update-button').on('click', function () {
+          // 정보수정 이동
+          const id = doc.id;
+          window.location.href = `albaUpdate.html?idvalue=${id}`;
+          $('.modal-container').fadeOut();
+        });
+        $('.close-button').on('click', function () {
+          // 모달창 닫기
+          $('.modal-container').fadeOut();
+        });
+      }
+    })
+    .catch((error) => {
+      console.error('Error getting document:', error);
+    });
+});
+// -------------------------------------------------------------------------------------
+// 알바생 데이터 삭제
+$(document).ready(function () {
+  // 삭제 버튼 누르기
+  $(document).on('click', '.alba-delete-button', function () {
+    // 선택한 알바생 데이터의 id값 추출하여 배열에 저장
+    const selectedIds = [];
+
+    // 체크박스 확인
+    $('.alba-list-body input[type="checkbox"]:checked').each(async function () {
+      const $parentRow = $(this).closest('tr');
+      const phoneNum = $parentRow.find('.alba-phone p').text();
+      const collection = db.collection('albainfo');
+      const docRef = collection.where('연락처', '==', phoneNum);
+      const querySnapshot = await docRef.get();
+
+      if (querySnapshot.size > 0) {
+        const doc = querySnapshot.docs[0];
+        const id = doc.id;
+        selectedIds.push(id);
+      }
+      // 모달창 띄우기 조건
+      if (selectedIds.length > 0) {
+        $('.alba-delete-button').prop('disabled', false); // 활성화
+        $('.confirm-modal-container').fadeIn();
+      } else {
+        $('.alba-delete-button').prop('disabled', true); // 비활성화
+      }
+
+      // 삭제하기 버튼 클릭 이벤트 리스너 등록
+      $('.confirm-button').on('click', async function () {
+        // 선택한 알바생 데이터 삭제 처리
+        const deletePromises = selectedIds.map((id) => {
+          return db.collection('albainfo').doc(id).delete();
+        });
+
+        // 삭제 Promise 모두 완료될 때까지 기다림
+        await Promise.all(deletePromises);
+        window.location.href = 'albaSelect.html';
+      });
+    });
+  });
+});
+// 모달창 닫기
+$('.cancel-button').on('click', async function () {
+  $('.confirm-modal-container').fadeOut();
 });
