5e62abdb2b717451fc923874bc90eaa681c3f92f
turkey-kim <rlaxmrgml@naver.com>
2023-08-17 00:13:56 +0900

    feat: 프로필 페이지 주요 DOM 상수화 2023.08.17

---
+++ js/constant/profile.js
@@ -0,0 +1,14 @@
+// 프로필 페이지 메인태그
+export const main = document.getElementById("profile-main");
+
+// 프로필 상세 컨테이너
+export const profile = document.getElementsByClassName("profile")[0];
+export const manageBar = document.getElementsByClassName("manage-bar")[0]; // 수정 및 삭제 버튼 컨테이너
+export const openEditorBtn = document.getElementById("manage-bar__edit-btn");
+export const removeBtn = document.getElementById("manage-bar__remove-btn");
+
+// 프로필 수정 폼
+export const editForm = document.getElementsByClassName("edit-form")[0];
+export const previewImg = document.getElementById("preview");
+export const submit = document.getElementsByClassName("edit-form__submit")[0];
+export const cancel = document.getElementsByClassName("edit-form__cancel")[0];
