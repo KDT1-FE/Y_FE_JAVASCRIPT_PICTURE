c3ff2df35a3542db3ce7d1029f7fe8637ff40705
89882 <pgh123166@naver.com>
2023-08-16 15:31:52 +0900

    feat: 스켈레톤 구현

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -5,26 +5,41 @@ import { getMemberDetail } from '../store/memberStore';
 
 export default class Detail extends Component {
   async render() {
-    const member = await getMemberDetail(getUrlParam('id')); // 현재 url의 아이디를 가지고 member의 상세 데이터를 받아옴
     this.el.innerHTML = `
-  <main class="detail">
-    <div class='photo-detail' style="background-image: url(${member.photoUrl})"></div>
+    <main class="detail">
+    <div class='photo-detail skeleton'></div>
     <section class='information-container'>
       <section class='information-title'>
         INFORMATION
         <button class='navigate-edit'>수정</button>
       </section>
-      <p class='information'>
-        <span class='detail-category'>NAME</span>
-        <span class="detail-value">${member.name}</span>
+      <p class='information skeleton'>
       </p>
-      <p class='information'>
-        <span class='detail-category'>EMAIL</span>
-        <span class="detail-value">${member.email}</span>
+      <p class='information skeleton'>
       </p>
-    </section>
-  </main> 
-    `;
+      </section>
+      </main>`;
+    this.el.prepend(new Header().el);
+    const member = await getMemberDetail(getUrlParam('id')); // 현재 url의 아이디를 가지고 member의 상세 데이터를 받아옴
+    this.el.innerHTML = `
+    <main class="detail">
+      <div class='photo-detail' style="background-image: url(${member.photoUrl})"></div>
+      <section class='information-container'>
+        <section class='information-title'>
+          INFORMATION
+          <button class='navigate-edit'>수정</button>
+        </section>
+        <p class='information'>
+          <span class='detail-category'>NAME</span>
+          <span class="detail-value">${member.name}</span>
+        </p>
+        <p class='information'>
+          <span class='detail-category'>EMAIL</span>
+          <span class="detail-value">${member.email}</span>
+        </p>
+      </section>
+    </main>
+      `;
     this.el.prepend(new Header().el);
     const navigateEditButton = this.el.querySelector('button');
     navigateEditButton.addEventListener('click', () =>
