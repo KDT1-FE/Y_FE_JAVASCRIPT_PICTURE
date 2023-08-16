7c8676279f624d183352c271ec7a37654cdd2ad8
JinJeongMin <jdsjm2@naver.com>
2023-08-16 12:46:47 +0900

    Feat: pages/main.js 데이터 가져오기 구현

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,7 +1,35 @@
-function Main() {
-  document.querySelector("#app").innerHTML = `
-                <div>메인 페이지</div>
-            `;
+import { app } from "../utils/db.js";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  query
+} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
+
+async function Main() {
+  const db = getFirestore(app);
+  const q = await getDocs(collection(db, "employee"));
+
+  const divApp = document.getElementById("app");
+
+  const listContainer = document.createElement("ul");
+  listContainer.setAttribute("class", "list-container");
+
+  divApp.append(listContainer);
+
+  // 데이터 받아와서 ul(list-container) > li(list-wrapper) > [ul > li]로 담아서 줌
+  q.forEach(doc => {
+    const listWrapper = document.createElement("li");
+    listWrapper.setAttribute("class", "list-wrapper");
+
+    listWrapper.innerHTML = `
+      <ul>
+        <li>${doc.data().name}</li>
+        <li>${doc.data().division}</li>
+      </ul>
+    `;
+    listContainer.append(listWrapper);
+  });
 }
 
 export default Main;
