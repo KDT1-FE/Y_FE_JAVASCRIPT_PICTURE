310d935337e011c23451e14079917ba5719f977a
xxxjinn <tlsguswls319@naver.com>
2023-08-16 13:38:23 +0900

    fix: img url로 가져오도록 수정

--- public/src/profile.js
+++ public/src/profile.js
@@ -7,27 +7,26 @@ const profileInfoUl = document.querySelector(".profile-info-ul");
 const db = firebase.firestore();
 const villagerDocRef = db.collection("villager").doc(villagerId);
 
-villagerDocRef
-  .get()
-  .then((doc) => {
-    if (doc.exists) {
-      const data = doc.data();
+villagerDocRef.get().then((doc) => {
+  if (doc.exists) {
+    const data = doc.data();
 
-      const name = data.name;
-      const engName = data.engName;
-      const sex = data.sex;
-      const birthday = data.birthday;
-      const personality = data.personality;
-      const favoriteColor = data.favoriteColor;
-      const speechHabit = data.speechHabit;
+    const name = data.name;
+    const engName = data.engName;
+    const sex = data.sex;
+    const birthday = data.birthday;
+    const personality = data.personality;
+    const favoriteColor = data.favoriteColor;
+    const speechHabit = data.speechHabit;
+    const imageUrl = data.imageUrl;
 
-      // ul 새로 만들기
-      const profileContainer = document.createElement("ul");
-      profileContainer.classList.add("villager-info");
-      profileContainer.dataset.id = doc.id;
+    // ul 새로 만들기
+    const profileContainer = document.createElement("ul");
+    profileContainer.classList.add("villager-info");
+    profileContainer.dataset.id = doc.id;
 
-      // 어떤 구조로 들어갈지
-      profileContainer.innerHTML = `
+    // 어떤 구조로 들어갈지
+    profileContainer.innerHTML = `
         <li class="villager-info-li"><p>${name}</p></li>
         <li class="villager-info-li"><p>${engName}</p></li>
         <li class="villager-info-li"><p>${sex}</p></li>
@@ -37,26 +36,13 @@ villagerDocRef
         <li class="villager-info-li"><p>"${speechHabit}"</p></li>
     `;
 
-      // div 추가
-      profileInfoUl.appendChild(profileContainer);
+    // div 추가
+    profileInfoUl.appendChild(profileContainer);
 
-      // img 가져오기
-      const imageFormats = ["webp", "png", "jpeg", "jpg"];
-      const imageElement = document.getElementById("villager-img");
-      let imageFound = false;
-
-      imageFormats.forEach((format) => {
-        if (!imageFound) {
-          const imageRef = storage.ref().child(`${engName}.${format}`);
-          imageRef
-            .getDownloadURL()
-            .then((url) => {
-              imageElement.src = url;
-              imageFound = true;
-            })
-            .catch((error) => {});
-        }
-      });
+    // img 가져오기
+    const imageElement = document.getElementById("villager-img");
+    if (imageUrl) {
+      imageElement.src = imageUrl;
     }
-  })
-  .catch((error) => {});
+  }
+});
