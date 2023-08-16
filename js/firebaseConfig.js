122efd6e40f543aacdfd75512991612101edec6f
suehub <dldustn2040@gmail.com>
2023-08-16 18:17:27 +0900

    Feat: Get Checked Email

---
+++ js/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA0BYrAIfJUyc09tEbBSZkRmDW6pRerOG8",
+  authDomain: "employee-management-59973.firebaseapp.com",
+  projectId: "employee-management-59973",
+  storageBucket: "employee-management-59973.appspot.com",
+  messagingSenderId: "541264054170",
+  appId: "1:541264054170:web:2e032d0b49e158016f9c23",
+  measurementId: "G-WFS0KJ2Y3C",
+};
+
+firebase.initializeApp(firebaseConfig);
--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 const db = firebase.firestore();
 const employeeTable = document.querySelector(".employee-table");
 const employeeAddBtn = document.querySelector(".employee-add__button");
-const employeeDeleteBtn = document.querySelector(".employee-remove__button");
+const employeeDeleteBtn = document.querySelector(".employee-delete__button");
 const modal = document.querySelector(".modal__background");
 const closeIcon = document.querySelector(".close-icon");
 const employeeForm = document.querySelector("#employee__form");
 const submitBtn = document.querySelector(".submit__button");
+const delCheckbox = document.querySelector(".checkbox");
 
 readEmployee();
 createEmployee();
+deleteEmployee();
+
+let checkedArr = new Array();
 
 employeeAddBtn.addEventListener("click", () => {
   modal.style.display = "block";
@@ -18,7 +22,7 @@ closeIcon.addEventListener("click", () => {
   modal.style.display = "none";
 });
 
-// firebase read
+// employee data read
 function readEmployee() {
   db.collection("직원")
     .get()
@@ -29,7 +33,7 @@ function readEmployee() {
         const checkbox = document.createElement("input");
         checkbox.type = "checkbox";
         checkbox.className = "checkbox";
-        checkbox.onclick = checkBg(checkbox);
+        checkbox.onclick = checked(checkbox);
         inputTd.append(checkbox);
         const imgTd = document.createElement("td");
         const img = document.createElement("img");
@@ -43,12 +47,15 @@ function readEmployee() {
         const dateTime = document.createElement("td");
         dateTime.innerHTML = doc.data().입사날짜;
         employeeTable.append(tr);
-        tr.append(inputTd, imgTd, name, tel, email, dateTime);
+        tr.append(inputTd, imgTd, name, email, tel, dateTime);
       });
+    })
+    .catch((err) => {
+      console.log(err);
     });
 }
 
-// firebase create
+// employee data create
 function createEmployee() {
   employeeForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -63,6 +70,7 @@ function createEmployee() {
       전화번호: tel.value,
       입사날짜: date.value,
     };
+
     db.collection("직원")
       .add(employee)
       .then((result) => {
@@ -76,35 +84,44 @@ function createEmployee() {
   });
 }
 
-// checked에 따라 table row 색상 변화
-function checkBg(checkbox) {
+// checked에 따라 배경색 변화, 삭제 위한 직원 이메일 저장
+function checked(checkbox) {
   checkbox.addEventListener("change", () => {
-    console.log(checkbox.checked);
+    // checked에 따라 table row 색상 변화
     let td = checkbox.parentNode;
     let tr = td.parentNode;
-    tr.style.backgroundColor = checkbox.checked ? "#e17878" : "#fff";
+    tr.style.backgroundColor = checkbox.checked ? "#aaa" : "#fff";
     tr.style.color = checkbox.checked ? "white" : "black";
+
+    // checked에 따라 직원 이메일 배열에 추가 or 삭제
+    checkbox.checked
+      ? checkedArr.push(tr.children[3].innerHTML)
+      : checkedArr.pop();
+    console.log(checkedArr);
   });
 }
 
-// firebase update
-// bucket.doc("bucket_item").update({ name: 'duck2' });
+// employee data update
 
-// firebase delete
-// employeeDeleteBtn.addEventListener("click", () => {
-//   let rowData = new Array();
-//   let tdArr = new Array();
-//   const checkbox = document.querySelector(".checkbox");
-//   let delcheckbox = checkbox.checked;
-//   console.log(delcheckbox);
-
-//   checkbox.each((i) => {
-//     let tr = checkbox.parent().eq(i);
-//     let td = tr.children();
-
-//     rowData.push(tr.text());
-
-//     console.log(rowData);
-//   });
-//   // db.collection("직원").delete();
-// });
+// employee data delete
+function deleteEmployee() {
+  employeeDeleteBtn.addEventListener("click", () => {
+    const q = query(collection(db, "직원"), where("이메일", "in", "gmail"));
+    getDocs(q).forEach((doc) => {
+      console.log(doc.id, " : ", doc.data());
+    });
+    // 문서 삭제위해 id가져오기
+    // db.collection("직원")
+    //   .get()
+    //   .then((result) => {
+    //     result.forEach((doc) => {
+    //       console.log(doc.id);
+    //     });
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+    // await db.collection("직원").doc("zP1huHLskkd7mgI1S4qc").delete();
+    // window.location.href = "./index.html";
+  });
+}
