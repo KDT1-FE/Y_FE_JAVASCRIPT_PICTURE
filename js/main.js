f4a40f32c48307eddf5dc572f219da07e108b2a0
suehub <dldustn2040@gmail.com>
2023-08-16 21:52:33 +0900

    Feat: Add Delete Employee

--- js/main.js
+++ js/main.js
@@ -5,19 +5,18 @@ const employeeDeleteBtn = document.querySelector(".employee-delete__button");
 const modal = document.querySelector(".modal__background");
 const closeIcon = document.querySelector(".close-icon");
 const employeeForm = document.querySelector("#employee__form");
-const submitBtn = document.querySelector(".submit__button");
-const delCheckbox = document.querySelector(".checkbox");
+const checkBox = document.querySelector(".checkbox");
+let checkedArr = new Array();
 
 readEmployee();
 createEmployee();
 deleteEmployee();
+checkedAll();
 
-let checkedArr = new Array();
-
+// 직원 등록 모달창 display
 employeeAddBtn.addEventListener("click", () => {
   modal.style.display = "block";
 });
-
 closeIcon.addEventListener("click", () => {
   modal.style.display = "none";
 });
@@ -28,6 +27,7 @@ function readEmployee() {
     .get()
     .then((result) => {
       result.forEach((doc) => {
+        // element 생성하고 data innerHTML로 작성
         const tr = document.createElement("tr");
         const inputTd = document.createElement("td");
         const checkbox = document.createElement("input");
@@ -46,6 +46,7 @@ function readEmployee() {
         email.innerHTML = doc.data().이메일;
         const dateTime = document.createElement("td");
         dateTime.innerHTML = doc.data().입사날짜;
+        // 테이블에 추가
         employeeTable.append(tr);
         tr.append(inputTd, imgTd, name, email, tel, dateTime);
       });
@@ -84,20 +85,59 @@ function createEmployee() {
   });
 }
 
+// 배열 중복 제거
+function delDupArr() {
+  let uniqueArr = [];
+  checkedArr.forEach((element) => {
+    if (!uniqueArr.includes(element)) {
+      uniqueArr.push(element);
+    }
+  });
+}
+
+// 전체 체크 설정 or 해제
+function checkedAll() {
+  const checkboxAll = document.querySelector(".checkbox__all");
+  checkboxAll.addEventListener("click", () => {
+    const isChecked = checkboxAll.checked;
+    const checkboxes = document.querySelectorAll(".checkbox");
+
+    if (isChecked) {
+      checkedArr = [];
+      for (const checkbox of checkboxes) {
+        checkbox.checked = true;
+        checkedArr.push(checkbox.parentNode.parentNode.children[3].innerHTML);
+        delDupArr();
+      }
+      checkedArr.shift();
+    } else {
+      for (const checkbox of checkboxes) {
+        checkbox.checked = false;
+        checkedArr = [];
+      }
+    }
+    console.log(checkedArr);
+  });
+}
+
 // checked에 따라 배경색 변화, 삭제 위한 직원 이메일 저장
 function checked(checkbox) {
   checkbox.addEventListener("change", () => {
-    // checked에 따라 table row 색상 변화
     let td = checkbox.parentNode;
     let tr = td.parentNode;
-    tr.style.backgroundColor = checkbox.checked ? "#aaa" : "#fff";
-    tr.style.color = checkbox.checked ? "white" : "black";
 
     // checked에 따라 직원 이메일 배열에 추가 or 삭제
     checkbox.checked
       ? checkedArr.push(tr.children[3].innerHTML)
-      : checkedArr.pop();
+      : (checkedArr = checkedArr.filter(
+          (element) => element !== `${tr.children[3].innerHTML}`
+        ));
+
     console.log(checkedArr);
+
+    // checked에 따라 table row 색상 변화
+    tr.style.backgroundColor = checkbox.checked ? "#aaa" : "#fff";
+    tr.style.color = checkbox.checked ? "white" : "black";
   });
 }
 
@@ -106,22 +146,20 @@ function checked(checkbox) {
 // employee data delete
 function deleteEmployee() {
   employeeDeleteBtn.addEventListener("click", () => {
-    const q = query(collection(db, "직원"), where("이메일", "in", "gmail"));
-    getDocs(q).forEach((doc) => {
-      console.log(doc.id, " : ", doc.data());
-    });
-    // 문서 삭제위해 id가져오기
-    // db.collection("직원")
-    //   .get()
-    //   .then((result) => {
-    //     result.forEach((doc) => {
-    //       console.log(doc.id);
-    //     });
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
-    // await db.collection("직원").doc("zP1huHLskkd7mgI1S4qc").delete();
-    // window.location.href = "./index.html";
+    for (let i = 0; i < checkedArr.length; i++) {
+      db.collection("직원")
+        .where("이메일", "==", `${checkedArr[i]}`)
+        .get()
+        .then((result) => {
+          result.forEach(async (doc) => {
+            await db.collection("직원").doc(`${doc.id}`).delete();
+            alert("직원 정보가 삭제되었습니다.");
+            window.location.href = "./index.html";
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
   });
 }
