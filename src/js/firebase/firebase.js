6ce95c41e1ea03404fecfa1e6dcafb74953e6010
NamgungJongMin <jmnamgung@gmail.com>
2023-08-17 13:59:32 +0900

    Feat: "Add action for signin page"
    Add page's action
    
    Connect page to firebase authentication

---
+++ src/js/firebase/firebase.js
@@ -0,0 +1,38 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js';
+import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-analytics.js';
+import { getAuth, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCy7QYDgKpkn-0eH42AmxAki6u4DH1oGZ0',
+  authDomain: 'members-dev-d3512.firebaseapp.com',
+  projectId: 'members-dev-d3512',
+  storageBucket: 'members-dev-d3512.appspot.com',
+  messagingSenderId: '277572932709',
+  appId: '1:277572932709:web:070862b87f629ee329a28c',
+  measurementId: 'G-3GBJM8LJ01',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+/* ------------------------------------ - ----------------------------------- */
+
+const auth = getAuth();
+
+const signinSubmit = (auth, email, password) => {
+  signInWithEmailAndPassword(auth, email, password)
+    .then(userCredential => {
+      // Signed in
+      const user = userCredential.user;
+
+      location.replace('main.html');
+
+      // ...
+    })
+    .catch(error => {
+      alert(['email, password를 다시 한번 확인해주세요']);
+    });
+};
+
+export { auth, signinSubmit };
