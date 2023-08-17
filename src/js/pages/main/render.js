fc6d5be469276fe8a4c31d1628a25e5b05a6ddfa
NamgungJongMin <jmnamgung@gmail.com>
2023-08-18 02:10:40 +0900

    Chore: "Delete comment in render function"

--- src/js/pages/main/render.js
+++ src/js/pages/main/render.js
@@ -1,27 +1,5 @@
 const $list = document.getElementById('member-list');
 
-// const render = async (members, isSignin = false) => {
-//   members.forEach(member => {
-//     $list.innerHTML += `
-//           <li class="my-4 flex h-40 cursor-pointer flex-row gap-8 bg-slate-200 pl-4 hover:bg-blue-200">
-//             <input type="checkbox" name="${member.email}" />
-//             <img
-//               class="object-contain"
-//               src="${member.profileUrl}"
-//               alt="${member.person}의 프로필 사진" />
-//             <div class="md:flex-row flex grow flex-col items-center justify-around text-center text-lg">
-//               <div class="md:basis-1/4 grow pt-1">${member.person}</div>
-//               <div class="md:hidden h-0.5 w-full bg-white"></div>
-//               <div class="md:basis-1/4 grow pt-1">${member.email}</div>
-//               <div class="md:hidden h-0.5 w-full bg-white"></div>
-//               <div class="md:basis-1/4 grow pt-1">${member.contact}</div>
-//               <div class="md:hidden h-0.5 w-full bg-white"></div>
-//               <div class="md:basis-1/4 grow pt-1">${member.division}</div>
-//             </div>
-//           </li>`;
-//   });
-// };
-
 // prettier-ignore
 const render = async (members, isSignin = false) => {
   $list.innerHTML = members.map(member => 
