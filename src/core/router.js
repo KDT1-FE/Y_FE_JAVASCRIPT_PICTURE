28282edfdd1d57da4c2c558cd5fb1747a5505d3a
89882 <pgh123166@naver.com>
2023-08-17 13:41:38 +0900

    fix: router 정규식 수정

--- src/core/router.js
+++ src/core/router.js
@@ -6,9 +6,10 @@ export function routeRender() {
   }
   const routerView = document.querySelector('router-view');
   const [hash, queryString] = location.hash.split('?');
-  const currentRoute = routes.find((route) =>
-    new RegExp(`${route.path}/?$`).test(hash)
-  );
+  const currentRoute = routes.find((route) => {
+    console.log(route.path);
+    return new RegExp(route.path + '/?$').test(hash);
+  });
   routerView.innerHTML = '';
   routerView.append(new currentRoute.component().el);
   window.scrollTo(0, 0);
