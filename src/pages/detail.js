00b6c839c317566484d6a1fc1b18ab685385cddc
JinJeongMin <jdsjm2@naver.com>
2023-08-17 16:56:03 +0900

    Feat: detail.js DOM 재설계, detail.css 구체화

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -20,20 +20,107 @@ async function Detail() {
   const docSnap = await getDoc(docRef);
 
   const divApp = document.getElementById("app");
-  divApp.innerHTML = `
-  <img src="${docSnap.data().image}"/>
-  <input type="text" name="name" id="name" value=${
-    docSnap.data().name
-  } disabled/>
-  <input type="text" name="position" id="team" value=${
-    docSnap.data().team
-  } disabled/>
-  <input type="text" name="team" id="position" value=${
-    docSnap.data().position
-  } disabled/>
-  <a href ="/edit/${path}">수정하기</a>
-  <button id="detail-delete">삭제하기</button>
-`;
+  divApp.innerHTML = "";
+
+  const detailContainer = document.createElement("div");
+  detailContainer.setAttribute("class", "detail-container");
+  divApp.append(detailContainer);
+
+  const detailTitleContainer = document.createElement("div");
+  detailTitleContainer.setAttribute("class", "detail-title-container");
+  detailContainer.append(detailTitleContainer);
+
+  const detailContentContainer = document.createElement("div");
+  detailContentContainer.setAttribute("class", "detail-content-container");
+  detailContainer.append(detailContentContainer);
+
+  // detail 타이틀 및 텍스트 생성
+  const detailTitleContainerTitle = document.createElement("h2");
+  detailTitleContainerTitle.setAttribute("class", "detail-title__title");
+  detailTitleContainerTitle.innerHTML = `대한민국 선수 관리 DB`;
+  detailTitleContainer.append(detailTitleContainerTitle);
+
+  const detailTitleContainerText = document.createElement("p");
+  detailTitleContainerText.setAttribute("class", "detail-title__p");
+  detailTitleContainerText.innerHTML = `대한민국 선수 상세 페이지입니다.`;
+  detailTitleContainer.append(detailTitleContainerText);
+
+  // detail 페이지 - content
+  const detailContentWrapper = document.createElement("div");
+  detailContentWrapper.setAttribute("class", "detail-content-wrapper");
+  detailContentContainer.append(detailContentWrapper);
+
+  // detail 페이지 - content - title
+  const detailContentTitleWrapper = document.createElement("div");
+  detailContentTitleWrapper.setAttribute(
+    "class",
+    "detail-content-title-wrapper"
+  );
+  detailContentWrapper.append(detailContentTitleWrapper);
+
+  const detailTitle = document.createElement("h3");
+  detailTitle.setAttribute("class", "detail-content-title_title");
+  detailTitle.innerHTML = "선수 등록";
+  detailContentTitleWrapper.append(detailTitle);
+
+  const detailBtnWrapper = document.createElement("div");
+  detailBtnWrapper.setAttribute("class", "detail-content-btn-wrapper");
+  detailContentTitleWrapper.append(detailBtnWrapper);
+
+  const detailEditBtn = document.createElement("a");
+  detailEditBtn.setAttribute("id", "edit");
+  detailEditBtn.href = `/edit/${path}`;
+  detailEditBtn.innerHTML = `수정하기`;
+  detailBtnWrapper.append(detailEditBtn);
+
+  const detailDeletBtn = document.createElement("button");
+  detailDeletBtn.setAttribute("id", "detail-delete");
+  detailDeletBtn.innerHTML = "삭제하기";
+  detailBtnWrapper.append(detailDeletBtn);
+
+  //detail 페이지 - content - input
+  const detailContentInputwrapper = document.createElement("div");
+  detailContentInputwrapper.setAttribute(
+    "class",
+    "detail-content-input-wrapper"
+  );
+  detailContentWrapper.append(detailContentInputwrapper);
+
+  detailContentInputwrapper.innerHTML = `
+      <div>
+      <span>이름</span>
+      <input type="text" name="name" id="name" value="${
+        docSnap.data().name
+      }" disabled/>
+      </div>
+      <div>
+      <span>팀명</span>
+      <input type="text" name="team" id="team" value="${
+        docSnap.data().team
+      }" disabled/>
+            </div>
+      <div>
+      <span>포지션</span>
+      <input type="text" name="team" id="position" value="${
+        docSnap.data().position
+      }" disabled/>
+      </div>
+      <div>
+      <span>사진</span>
+      <input type="file" id="image" disabled/>
+  `;
+
+  // detail 페이지 - img
+  const detailContentImageWrapper = document.createElement("div");
+  detailContentImageWrapper.setAttribute(
+    "class",
+    "detail-content-image-wrapper"
+  );
+  detailContentContainer.append(detailContentImageWrapper);
+
+  detailContentImageWrapper.innerHTML = `
+  <img id = "detail-myimg"src="${docSnap.data().image}"/>
+  `;
 
   document
     .querySelector("#detail-delete")
