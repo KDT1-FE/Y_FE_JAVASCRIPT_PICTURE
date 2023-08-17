031bc31aefc786650b1a47255a73261070f56b9f
JitHoon <imessi0336@gmail.com>
2023-08-18 02:53:24 +0900

    Env: Import confirm page components

---
+++ src/js/pages/confirm.js
@@ -0,0 +1,7 @@
+// import common components
+import "../components/load.js";
+import "../components/clickBackBtn";
+
+// import confirm components
+import "../components/confirm/getAccidentProfile.js";
+import "../components/confirm/clickConfirm.js";
