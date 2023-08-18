998553330fc9dc403cf60a29beba48b997d57978
humnyenye <jisong0292@gmail.com>
2023-08-18 17:20:46 +0900

    Feat: 전체적 디자인 수정 및 기능 수정

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -10,3 +10,58 @@ window.addEventListener("DOMContentLoaded", (event) => {
     dateBox.querySelector(".time").textContent = formattedTime;
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const navMainItems = document.querySelectorAll(".nav__main > li > .nav__main__text");
+  const navSubItems = document.querySelectorAll(".nav__sub > li");
+
+  navMainItems.forEach((item) => {
+    item.addEventListener("click", function () {
+      navMainItems.forEach((li) => li.parentElement.classList.remove("nav__main--active"));
+      navSubItems.forEach((li) => li.classList.remove("nav__sub--active"));
+
+      const parentLi = this.parentElement;
+      parentLi.classList.add("nav__main--active");
+
+      // nav__sub가 있을 경우 첫 번째 항목에 active 클래스 추가
+      const firstSubItem = parentLi.querySelector(".nav__sub > li");
+      if (firstSubItem) {
+        firstSubItem.classList.add("nav__sub--active");
+      }
+    });
+  });
+
+  navSubItems.forEach((item) => {
+    item.addEventListener("click", function () {
+      navSubItems.forEach((li) => li.classList.remove("nav__sub--active"));
+      navMainItems.forEach((li) => li.parentElement.classList.remove("nav__main--active"));
+      this.classList.add("nav__sub--active");
+      this.closest(".nav__main > li").classList.add("nav__main--active");
+    });
+  });
+});
+
+document.querySelector(".logo").addEventListener("click", function () {
+  window.location.href = "index.html";
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const aside = document.querySelector("aside");
+  const asideOpner = document.querySelector(".icon__menu");
+  const asideCloser = document.querySelector(".aside-closer");
+
+  asideOpner.addEventListener("click", function () {
+    console.log("click");
+    aside.style.left = "0%";
+  });
+
+  asideCloser.addEventListener("click", function () {
+    aside.style.left = "-100%";
+  });
+
+  document.body.addEventListener("click", function (event) {
+    if (!aside.contains(event.target) && event.target !== asideOpner) {
+      aside.style.left = "-100%";
+    }
+  });
+});
