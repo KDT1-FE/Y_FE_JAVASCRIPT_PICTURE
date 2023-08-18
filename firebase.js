e213f59b66ef211c289aecc137dddee6dbd2148c
Eojoonhyuk <anftlcb@gmail.com>
2023-08-18 09:34:19 +0900

    firebase.js 수정

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDZNtKbE6q5-ROZ6rnCa14zCA_xnD15naI",
+  authDomain: "manage-workes.firebaseapp.com",
+  projectId: "manage-workes",
+  storageBucket: "manage-workes.appspot.com",
+  messagingSenderId: "594002563664",
+  appId: "1:594002563664:web:3d620f0a7c69421300b755",
 };
 
 const app = initializeApp(firebaseConfig);
