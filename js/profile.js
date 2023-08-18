49d74dce0aabf20f6661a95ed4f903260ebaf3a0
lilviolie <lilviolie@gmail.com>
2023-08-18 21:46:25 +0900

    Fix the modal design

--- js/profile.js
+++ js/profile.js
@@ -22,6 +22,12 @@ const $modalBackground = document.querySelector('.modal--background');
 const $modify = document.querySelector('.button--modify');
 const $delete = document.querySelector('.button--delete');
 
+window.addEventListener('scroll', () => {
+  const $scrollY = window.scrollY;
+  console.log($scrollY);
+  $modalBackground.style.top = `${$scrollY}px`;
+});
+
 $modify.addEventListener('click', () => {
   $modalBackground.classList.toggle('show');
   document.querySelector('.update').innerHTML = `
@@ -90,6 +96,7 @@ $delete.addEventListener('click', () => {
 
   document.querySelector('.button--no').addEventListener('click', () => {
     $modalBackground.classList.toggle('show');
+    $body.style.overflow = 'auto';
   });
 
   if ($modalBackground.classList.contains('show')) {
