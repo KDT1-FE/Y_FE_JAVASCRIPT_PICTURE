e1fe1003e49469108923682c92f81ed00c1c57c3
developer-jyyun <developer.jyyun@gmail.com>
2023-08-18 21:25:37 +0900

    Feat:Add loading page

---
+++ public/loading.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+    setTimeout(()=>{
+    loading.style.opacity = "0";    
+    loading.style.transform = "scale(0)";    
+    loading.style.transition='all 1s';   
+    
+    },1200);
+}
+    
+
+
