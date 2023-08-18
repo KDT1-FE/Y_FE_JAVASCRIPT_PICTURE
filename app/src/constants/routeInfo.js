d8a4acc0b39295e3894b8d46c0ad472af14f8cc1
JinJeongMin <jdsjm2@naver.com>
2023-08-18 11:19:27 +0900

    Feat: routeInfo.js BASE_URL 스스로 받아오게 수정

--- app/src/constants/routeInfo.js
+++ app/src/constants/routeInfo.js
@@ -4,7 +4,7 @@ import Edit from "../pages/edit.js";
 import Detail from "../pages/detail.js";
 
 // 베이스 URL 저장 => 나중에 도메인으로 바꿔주거나, URL 긁어오는 방법 찾아서 자동 할당
-export const BASE_URL = "http://localhost:1234";
+export const BASE_URL = window.location.origin;
 
 // 정규표현식을 활용해 원하는 element 반환
 export const routes = [
