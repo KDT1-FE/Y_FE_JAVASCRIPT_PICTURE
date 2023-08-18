6043fe84bf588b8407d1e4e0a302a65949232f2c
flamozzi <pjs99047@gmail.com>
2023-08-18 09:01:46 +0900

    Feat: Update Key file & gitignore

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
 import { firebaseConfig } from './firebase-config.js';
 
 // Firebase 설정
+var firebaseConfig = {
+  apiKey: process.env.REACT_APP_FB_API_KEY,
+  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FB_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FB_MESSAGE_ID,
+  appId: process.env.REACT_APP_FB_APP_ID,
+  measurementId: process.env.REACT_APP_FB_MEASUREMENT_ID,
+};
 firebase.initializeApp(firebaseConfig);
 
 // Database와 Storage 참조
