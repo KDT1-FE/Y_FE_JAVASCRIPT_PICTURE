f592d1c075904ce7b0847249fa49fd5247d56cf3
mysdpy30s <mysdpy30s@gmail.com>
2023-08-18 23:33:03 +0900

    Update: Final project

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,6 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
 import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-storage.js";
+import {
   getFirestore,
   collection,
   addDoc,
@@ -26,6 +32,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore();
+const storage = getStorage(app);
 
 // CRUD CONTROL FUNCTION
 
@@ -46,3 +53,9 @@ export const getData = (id) => getDoc(doc(db, "myplants", id));
 export const updateData = (id, newInfo) =>
   updateDoc(doc(db, "myplants", id), newInfo);
 //export const editData = (id) => (doc(db, 'myplants', id))
+
+// UPLOAD FILE
+
+export const storageRef = (imageFile) => ref(storage, imageFile);
+export const uploadTask = (storageRef, imageFile) =>
+  uploadBytesResumable(storageRef, imageFile);
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,28 @@
 // CALLING FUNCTIONS
 import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-storage.js";
+import {
   saveData,
   onGetData,
   deleteData,
   getData,
   updateData,
+  storageRef,
+  uploadTask,
 } from "./firebase.js";
 
 const plantsForm = document.getElementById("plants-form");
 const plantsDataContainer = document.getElementById("plants-data-container");
 const table = document.querySelector(".list__actual table");
+
+// Handle image upload form submission
+const imageInput = document.getElementById("imageInput");
+const imageContainer = document.getElementById("imageContainer");
+const progress = document.getElementById("progress");
 
 let editStatus = false;
 let id = ""; //db ID for elements
@@ -28,7 +41,6 @@ openModalButton.addEventListener("click", () => {
 
 // Function to open modal and pre-fill with data
 function openEditModal(data) {
-  plantsForm["image-data"].value = data.image;
   plantsForm["name-data"].value = data.name;
   plantsForm["date-data"].value = data.date;
   plantsForm["water-time-data"].value = data.waterTime;
@@ -49,20 +61,20 @@ window.addEventListener("DOMContentLoaded", async () => {
     querySnapshot.forEach((doc) => {
       const plantData = { id: doc.id, ...doc.data() };
       html += `
-                  <div>
-                      <table>
-                          <tr>
-                              <td>${plantData.image}</td>
-                              <td>${plantData.name}</td>
-                              <td>${plantData.date}</td>
-                              <td>${plantData.waterTime}</td>
-                              <td>${plantData.note}</td>
-                              <td><button class='btn-edit' data-id="${doc.id}">수정</button></td>
-                              <td><button class='btn-delete' data-id="${doc.id}">삭제</button></td>
-                          </tr>
-                      </table>
-                  </div>
-              `;
+                <div>
+                    <table>
+                        <tr>
+                            <td><img src="${plantData.image}" class="plant-image"></td>
+                            <td>${plantData.name}</td>
+                            <td>${plantData.date}</td>
+                            <td>${plantData.waterTime}</td>
+                            <td>${plantData.note}</td>
+                            <td><button class='btn-edit' data-id="${doc.id}">수정</button></td>
+                            <td><button class='btn-delete' data-id="${doc.id}">삭제</button></td>
+                        </tr>
+                    </table>
+                </div>
+            `;
     });
     // contains all the elementes
     plantsDataContainer.innerHTML = html;
@@ -85,7 +97,9 @@ window.addEventListener("DOMContentLoaded", async () => {
         const data = { id: doc.id, ...doc.data() };
         openEditModal(data);
         //bring  values from db
-        plantsForm["image-data"].value = data.image;
+        //plantsForm["printImage"].value = data.image;
+        const imgSrc = document.getElementById("printImage");
+        imgSrc.src = data.image;
         plantsForm["name-data"].value = data.name;
         plantsForm["date-data"].value = data.date;
         plantsForm["water-time-data"].value = data.waterTime;
@@ -97,67 +111,120 @@ window.addEventListener("DOMContentLoaded", async () => {
     });
 
     // SAVE
-    plantsForm.addEventListener("submit", (e) => {
+    plantsForm.addEventListener("submit", async (e) => {
       e.preventDefault();
 
-      const image = plantsForm["image-data"];
       const name = plantsForm["name-data"];
       const date = plantsForm["date-data"];
       const waterTime = plantsForm["water-time-data"];
       const note = plantsForm["note-data"];
 
-      if (!editStatus) {
-        saveData(
-          image.value,
-          name.value,
-          date.value,
-          waterTime.value,
-          note.value
-        );
-        alert("저장되었습니다!");
+      //IMAGE SAVING
+      const imageFile = imageInput.files[0];
 
-        // Add a new row to the table
-        const newRow = document.createElement("tr");
-        newRow.innerHTML = `
-        <td>${image.value}</td>
-        <td>${name.value}</td>
-        <td>${date.value}</td>
-        <td>${waterTime.value}</td>
-        <td>${note.value}</td>
-        <td><button class='btn-edit' data-id="${doc.id}">수정</button></td>
-        <td><button class='btn-delete' data-id="${doc.id}">삭제</button></td>
-      `;
-        table.appendChild(newRow);
-      } else {
-        console.log("수정되었습니다!");
-        updateData(id, {
-          image: image.value,
-          name: name.value,
-          date: date.value,
-          waterTime: waterTime.value,
-          note: note.value,
-        });
-        editStatus = false;
-        plantsForm["btn-data-save"].innerText = "저장";
+      if (!imageFile) {
+        if (editStatus) {
+          updateData(id, {
+            name: name.value,
+            date: date.value,
+            waterTime: waterTime.value,
+            note: note.value,
+          });
+          editStatus = false;
+          plantsForm["btn-data-save"].innerText = "저장";
+          plantsForm.reset();
+          refresh();
 
-        // Update the edited row in the table
-        const editedRow = table.querySelector(`tr[data-id="${doc.id}"]`);
-        if (editedRow) {
-          editedRow.innerHTML = `
-        <td>${image.value}</td>
-        <td>${name.value}</td>
-        <td>${date.value}</td>
-        <td>${waterTime.value}</td>
-        <td>${note.value}</td>
-        <td><button class='btn-edit' data-id="${doc.id}">수정</button></td>
-        <td><button class='btn-delete' data-id="${doc.id}">삭제</button></td>
-      `;
+          return;
         }
+
+        alert("이미지를 추가하세요.");
+        return;
       }
 
-      plantsForm.reset();
-      // Close the modal
-      document.getElementById("myModal").style.display = "none";
+      const imageStorageRef = storageRef(imageFile.name);
+      const imageUploadTask = uploadTask(imageStorageRef, imageFile);
+
+      imageUploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const percent =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          progress.textContent = `Uploading: ${percent.toFixed(2)}%`;
+        },
+        (error) => {
+          console.error("Error uploading image:", error);
+        },
+        async () => {
+          progress.textContent = "Upload Complete!";
+          //refresh();
+          const downloadURL = await getDownloadURL(
+            imageUploadTask.snapshot.ref
+          );
+          //DATA SAVING
+          const image = downloadURL;
+          const name = plantsForm["name-data"];
+          const date = plantsForm["date-data"];
+          const waterTime = plantsForm["water-time-data"];
+          const note = plantsForm["note-data"];
+
+          if (!editStatus) {
+            saveData(
+              image,
+              name.value,
+              date.value,
+              waterTime.value,
+              note.value
+            );
+            alert("저장되었습니다!");
+            refresh();
+            // Add a new row to the table
+            const newRow = document.createElement("tr");
+            newRow.innerHTML = `
+      <td><img src="${image}" class="plant-image"></td>
+      <td>${name.value}</td>
+      <td>${date.value}</td>
+      <td>${waterTime.value}</td>
+      <td>${note.value}</td>
+      <td><button class='btn-edit' data-id="${doc.id}">수정</button></td>
+      <td><button class='btn-delete' data-id="${doc.id}">삭제</button></td>
+    `;
+            table.appendChild(newRow);
+          } else {
+            console.log("수정되었습니다!");
+            updateData(id, {
+              image: image,
+              name: name.value,
+              date: date.value,
+              waterTime: waterTime.value,
+              note: note.value,
+            });
+            editStatus = false;
+            plantsForm["btn-data-save"].innerText = "저장";
+
+            // Update the edited row in the table
+            const editedRow = table.querySelector(`tr[data-id="${doc.id}"]`);
+            if (editedRow) {
+              editedRow.innerHTML = `
+      <td><img src="${image}" class="plant-image"></td>
+      <td>${name.value}</td>
+      <td>${date.value}</td>
+      <td>${waterTime.value}</td>
+      <td>${note.value}</td>
+      <td><button class='btn-edit' data-id="${doc.id}">수정</button></td>
+      <td><button class='btn-delete' data-id="${doc.id}">삭제</button></td>
+    `;
+            }
+          }
+        }
+      );
     });
+    plantsForm.reset();
   });
 });
+
+function refresh() {
+  setTimeout(function () {
+    location.reload();
+  }, 2000);
+}
