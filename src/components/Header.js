7179b26d966a3477447bc05712539afde9f7c056
sunpaaaa <sunpaaaa@gmail.com>
2023-08-18 10:50:06 +0900

    Remove form element for search

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import Modal from './Modal.js';
-import SaveButton from './SaveButton.js';
 
 export default class Header {
   constructor() {
@@ -21,28 +20,15 @@ export default class Header {
     toggleModeBtnEl.classList.add('btn', 'toggle', 'material-symbols-outlined');
     toggleModeBtnEl.innerText = `${this.isDark ? 'dark_mode' : 'light_mode'} `;
 
-    const searchBtnEl = document.createElement('button');
-    searchBtnEl.classList.add('btn', 'search', 'material-symbols-outlined');
-    searchBtnEl.innerText = `search`;
-
     const addBtnEl = document.createElement('button');
     addBtnEl.classList.add('btn', 'add');
     addBtnEl.innerText = '새로운 멤버 추가';
 
-    btnsEl.append(toggleModeBtnEl, searchBtnEl, addBtnEl);
+    btnsEl.append(toggleModeBtnEl, addBtnEl);
 
     const topEl = document.createElement('div');
     topEl.classList.add('top');
     topEl.append(titleEl, btnsEl);
-
-    // Header bottom
-    const bottomEl = document.createElement('div');
-    bottomEl.classList.add('bottom', 'hide');
-
-    const searchInputEl = document.createElement('input');
-    searchInputEl.classList.add('input');
-    searchInputEl.setAttribute('placeholder', '검색어를 입력하세요.');
-    bottomEl.append(searchInputEl);
 
     // Event
     toggleModeBtnEl.addEventListener('click', () => {
@@ -56,16 +42,12 @@ export default class Header {
       this.render();
     });
 
-    searchBtnEl.addEventListener('click', () => {
-      bottomEl.classList.toggle('hide');
-    });
-
     addBtnEl.addEventListener('click', () => {
       const body = document.querySelector('body');
       const modal = new Modal({}, 'create');
       body.append(modal.el);
     });
 
-    this.el.append(topEl, bottomEl);
+    this.el.append(topEl);
   }
 }
