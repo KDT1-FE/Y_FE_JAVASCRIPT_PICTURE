3ca529ea9690e36bf029d8f82ef3cd2c81774cd9
Eojoonhyuk <anftlcb@gmail.com>
2023-08-18 10:35:07 +0900

    환경변수 수정

--- firebase.js
+++ firebase.js
@@ -1,21 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-const {
-  NEXT_PUBLIC_APT_KEY,
-  NEXT_PUBLIC_AUTH_DOMAIN,
-  NEXT_PUBLIC_PROJECT_ID,
-  NEXT_PUBLIC_STORAGE_BUCKET,
-  NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  NEXT_PUBLIC_APP_ID,
-} = process.env;
 const firebaseConfig = {
-  apiKey: NEXT_PUBLIC_APT_KEY,
-  authDomain: NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: NEXT_PUBLIC_APP_ID,
+  apiKey: "AIzaSyDZNtKbE6q5-ROZ6rnCa14zCA_xnD15naI",
+  authDomain: "manage-workes.firebaseapp.com",
+  projectId: "manage-workes",
+  storageBucket: "manage-workes.appspot.com",
+  messagingSenderId: "594002563664",
+  appId: "1:594002563664:web:3d620f0a7c69421300b755",
 };
 
 const app = initializeApp(firebaseConfig);
