42c71e616dc73fbeed76f30d5b0e8710c525aee4
developer-jyyun <developer.jyyun@gmail.com>
2023-08-19 17:08:52 +0900

    Fix: Loading

--- public/loading.js
+++ public/loading.js
@@ -1,10 +1,15 @@
 window.onload = function () {
+
+    const loading = document.getElementById("loading");
+
     setTimeout(()=>{
     loading.style.opacity = "0";    
     loading.style.transform = "scale(0)";    
     loading.style.transition='all 1s';   
     
     },1200);
+
+    console.log(loading)
 }
     
 
