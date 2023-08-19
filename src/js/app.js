e3e47db8c47e52d53d0eb77ae8d998c7f2f252e5
JitHoon <imessi0336@gmail.com>
2023-08-19 10:50:17 +0900

    Refactor: Hide firebaseConfig using dot env
    firebase web app 생성에 필요한 apiKey 등을 dotenv-webpack 플러그인을 사용해 보안을 강화하였습니다.

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app";
 
-// web app's Firebase configuration
+// Firebase web app 구성(configuration)
 const firebaseConfig = {
-  apiKey: "AIzaSyDzNDsZIB_wG7T_c7MmyNjIIjs3pXSqH6M",
-  authDomain: "zero-car.firebaseapp.com",
-  projectId: "zero-car",
-  storageBucket: "zero-car.appspot.com",
-  messagingSenderId: "283362652912",
-  appId: "1:283362652912:web:e5c5304b2ed65aeb958835",
-  measurementId: "G-7DNJ8CY84W",
-  storageBucket: "gs://zero-car.appspot.com"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+  storageBucket: process.env.STORRAGE_BUCKET
 };
 
 const app = initializeApp(firebaseConfig);
