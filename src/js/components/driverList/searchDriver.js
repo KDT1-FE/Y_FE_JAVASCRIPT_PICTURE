8987ad368117229fc9bccf2ccb8fab5cb9ed7c66
JitHoon <imessi0336@gmail.com>
2023-08-20 18:38:25 +0900

    Refactor: Clean searchDriver  code

--- src/js/components/driverList/searchDriver.js
+++ src/js/components/driverList/searchDriver.js
@@ -1,29 +1,38 @@
-// HTML document를 전부 읽고 DOM트리를 완성하는 즉시 이벤트가 호출
+// 보험자 리스트 페이지에서
+//table tbody의 tr 데이터 중 driverName을 검섹하는 Component
+
+// HTML document를 전부 읽고 DOM 트리를 완성하는 즉시 searchDriver 콜백함수 호출
 document.addEventListener("DOMContentLoaded", searchDriver);
 
 function searchDriver() {
-  // 검색창 element를 id값으로 가져오기
-  const searchDriverInput = document.querySelector("#searchDriver");
-  // 테이블의 tbody element를 id값으로 가져오기
-  const driversTbody = document.querySelector("#drivers");
+  try {
+    const searchDriverInput = document.getElementById("searchDriver");
+    const driversTbody = document.getElementById("drivers");
 
-  //검색창 element에 keyup 이벤트 세팅. 글자 입력 시 마다 발생.
-  searchDriverInput.addEventListener("keyup", () => {
-    // 사용자가 입력한 검색어의 value값을 가져와 소문자로 변경하여 searchInputValue에 저장
-    const searchInputValue = searchDriverInput.value.toLowerCase();
-    // 현재 tbody안에 있는 모든 tr element를 가져와 drivers에 저장
-    const drivers = driversTbody.querySelectorAll("tr");
+    if (searchDriverInput && driversTbody) {
+      //검색창 element에 keyup 이벤트 추가가
+      searchDriverInput.addEventListener("keyup", () => {
+        // 사용자가 입력한 검색어의 value값을 가져와 소문자로 변경 후 searchInputValue에 할당
+        const searchInputValue = searchDriverInput.value.toLowerCase();
+        // 현재 tbody안에 있는 모든 tr element를 가져와 drivers에 할당
+        const drivers = driversTbody.querySelectorAll("tr");
 
-    //tr들 for문으로 순회
-    for (var i = 0; i < drivers.length; i++) {
-      // 현재 순회중인 tr의 textContent를 소문자로 변경하여 driverName에 저장
-      var driverName = drivers[i].children[2].textContent.toLowerCase();
-      // driverName가 searchInputValue를 포함하면, 해당 tr은 보여지게 하고, 그렇지 않으면 숨긴다.
-      if (driverName.includes(searchInputValue)) {
-        drivers[i].style.display = "";
-      } else {
-        drivers[i].style.display = "none";
-      }
-    }
-  });
+        //tr들 for문으로 순회
+        for (var i = 0; i < drivers.length; i++) {
+          // 현재 순회중인 tr의 driverName 부분을 textContent를 소문자로 변경 후 driverName에 할당
+          var driverName = drivers[i].children[2].textContent.toLowerCase();
+          // driverName아 searchInputValue를 포함하면, 해당 tr은 보여지게 하고, 그렇지 않으면 숨긴다.
+          if (driverName.includes(searchInputValue)) {
+            drivers[i].style.display = "";
+          } else {
+            drivers[i].style.display = "none";
+          }
+        }
+      });
+    } else throw "404 페이지로";
+  } catch {
+    err => {
+      console.log(err);
+    };
+  }
 }
