2b2931f5a88d658ed3886e512ad240d27bc1e529
JitHoon <imessi0336@gmail.com>
2023-08-20 16:15:53 +0900

    Refactor: Remove not using component in driverList

--- src/js/driverList.js
+++ src/js/driverList.js
@@ -3,7 +3,6 @@ import "./components/load.js";
 
 // import driverList components
 import "./components/driverList/getDriverList.js";
-import "./components/driverList/clickCheckBox.js";
 import "./components/driverList/clickDeleteDriver.js";
 import "./components/driverList/deleteDriver.js";
 import "./components/driverList/searchDriver.js";
