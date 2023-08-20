5ac459ca4aacd0547f8a883d8332b58915d791ef
JitHoon <imessi0336@gmail.com>
2023-08-20 20:19:08 +0900

    Refactor: Clean clickBackBtn component

--- src/js/components/clickBackBtn.js
+++ src/js/components/clickBackBtn.js
@@ -1,6 +1,10 @@
+// addDriver, confirmAccident, driverProfile 페이지에서
+// driverList 로 돌아가는 버튼 클릭 이벤트 component
 const backBtn = document.querySelector("#back button");
 
-backBtn.addEventListener("click", goToDriverListPage);
+backBtn
+  ? backBtn.addEventListener("click", goToDriverListPage)
+  : console.log("404 페이지로");
 
 function goToDriverListPage() {
   location.href = "./driverList.html";
