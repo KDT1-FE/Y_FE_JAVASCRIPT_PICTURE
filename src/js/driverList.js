9356b1191f7946f89e1c7a77a27d593fcfea3593
JitHoon <imessi0336@gmail.com>
2023-08-20 18:39:39 +0900

    Refactor: Delete not use Import url in driverList

--- src/js/driverList.js
+++ src/js/driverList.js
@@ -4,6 +4,5 @@ import "./components/load.js";
 // import driverList components
 import "./components/driverList/getDriverList.js";
 import "./components/driverList/clickDeleteDriver.js";
-import "./components/driverList/deleteDriver.js";
 import "./components/driverList/searchDriver.js";
 import "./components/driverList/clickAddDriver.js";
