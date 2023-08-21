c792d2b376526160a083d5f90f61f82320b5d0e2
imnayoung <myblu1008@gmail.com>
2023-08-22 01:47:44 +0900

    Fix: Update .env

--- src/public/js/common/firebase.js
+++ src/public/js/common/firebase.js
@@ -4,11 +4,11 @@ import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY || 'AIzaSyC23x__H9jNFl4vGL8Bo0u5qp4W4J97iUI',
-  apiKey: process.env.FIREBASE_API_KEY || 'crew-control-service.firebaseapp.com',
-  apiKey: process.env.FIREBASE_API_KEY || 'crew-control-service',
-  apiKey: process.env.FIREBASE_API_KEY || 'crew-control-service.appspot.com',
-  apiKey: process.env.FIREBASE_API_KEY || '1009210883696',
-  apiKey: process.env.FIREBASE_API_KEY || '1:1009210883696:web:982978404e4f1edc163eb5',
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN || 'crew-control-service.firebaseapp.com',
+  projectId: process.env.FIREBASE_PROJECT_ID || 'crew-control-service',
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || 'crew-control-service.appspot.com',
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || '1009210883696',
+  appId: process.env.FIREBASE_APP_ID || '1:1009210883696:web:982978404e4f1edc163eb5',
 }
 
 const app = initializeApp(firebaseConfig)
