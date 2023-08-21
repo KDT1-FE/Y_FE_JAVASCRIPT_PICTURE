509222966fe4c51c0efb4a846979c6f444a5b7d8
imnayoung <myblu1008@gmail.com>
2023-08-22 01:44:42 +0900

    Feat: Add .env

--- src/public/js/common/firebase.js
+++ src/public/js/common/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC23x__H9jNFl4vGL8Bo0u5qp4W4J97iUI',
-  authDomain: 'crew-control-service.firebaseapp.com',
-  projectId: 'crew-control-service',
-  storageBucket: 'crew-control-service.appspot.com',
-  messagingSenderId: '1009210883696',
-  appId: '1:1009210883696:web:982978404e4f1edc163eb5',
+  apiKey: process.env.FIREBASE_API_KEY || 'AIzaSyC23x__H9jNFl4vGL8Bo0u5qp4W4J97iUI',
+  apiKey: process.env.FIREBASE_API_KEY || 'crew-control-service.firebaseapp.com',
+  apiKey: process.env.FIREBASE_API_KEY || 'crew-control-service',
+  apiKey: process.env.FIREBASE_API_KEY || 'crew-control-service.appspot.com',
+  apiKey: process.env.FIREBASE_API_KEY || '1009210883696',
+  apiKey: process.env.FIREBASE_API_KEY || '1:1009210883696:web:982978404e4f1edc163eb5',
 }
 
 const app = initializeApp(firebaseConfig)
