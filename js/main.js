63caa6e2052a8e4e6d7483a242d20e81adf22879
Hojin <wkdghwls0623@gmail.com>
2023-08-23 11:38:48 +0900

    fix bug

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 const toogleBtn = document.querySelector(".navbar__tooglebtn");
 
+function toggleClassOnElement(element, className) {
+  element?.classList.toggle(className);
+}
+
+const toggleNavbar = () => {
+  toggleClassOnElement(document.querySelector(".navbar__menu"), "active");
+  console.log("click");
+};
+
+toogleBtn.addEventListener("click", toggleNavbar);
+
 document.getElementById("image").addEventListener("change", function (e) {
   if (e.target.files && e.target.files[0]) {
     const reader = new FileReader();
