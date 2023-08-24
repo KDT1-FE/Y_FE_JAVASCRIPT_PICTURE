f1ed11217593bf3070654e5d451d234d4b3da7be
SOL-MI <ovo10203@gmail.com>
2023-08-24 22:10:14 +0900

    Comment: playground 주석 추가

--- pages/moreInfo/moreInfo.js
+++ pages/moreInfo/moreInfo.js
@@ -1,6 +1,23 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
-import { getStorage, ref, uploadBytes, getDownloadURL, deleteObject } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-storage.js";
-import { getFirestore, collection, getDocs, doc, orderBy, query, onSnapshot, deleteDoc, Timestamp, updateDoc } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  deleteObject,
+} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-storage.js";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  doc,
+  orderBy,
+  query,
+  onSnapshot,
+  deleteDoc,
+  Timestamp,
+  updateDoc,
+} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBXVgQW2Xq5fE1SvaVVutpTgX_6ZaotQhQ",
@@ -9,7 +26,7 @@ const firebaseConfig = {
   storageBucket: "photo-management-service.appspot.com",
   messagingSbuttonenderId: "110781159358",
   appId: "1:110781159358:web:e9b8fbdc3e60c979178bef",
-  measurementId: "G-E657JQPN7T"
+  measurementId: "G-E657JQPN7T",
 };
 
 // Initialize Firebase
@@ -18,7 +35,7 @@ const db = getFirestore(app);
 const storage = getStorage(app);
 
 //상세profile카드 관련 선언
-const profileContainer = document.querySelector('.detail-container');
+const profileContainer = document.querySelector(".detail-container");
 let template = `
     <div class="detail-profile-card">
       <img class="detail-profile-image" src="{{__profile_image__}}" alt="profile image">
@@ -36,78 +53,87 @@ let template = `
     </div>
 `;
 //상세profile카드 추가하기
-const q = query(collection(db,"profiles"),orderBy("date"))
+const q = query(collection(db, "profiles"), orderBy("date"));
 let itemNumber = 0;
 let clickCount = 0;
 
-onSnapshot(q,(querySnapshot) => {
+onSnapshot(q, (querySnapshot) => {
   querySnapshot.forEach((doc) => {
     //console.log('시작:',doc.data())
     const newProfile = document.createElement("div");
-    newProfile.setAttribute("id",doc.id)
+    newProfile.setAttribute("id", doc.id);
     const newProfileIndex = document.createElement("div");
     newProfile.classList.add(`detail-item`);
-    newProfileIndex.setAttribute("id",`item${itemNumber}`);
-    newProfileIndex.classList.add('detail-index');
-    newProfileIndex.innerHTML=`#${itemNumber+1}`;
-    template = template.replace('{{__profile_image__}}',doc.data().image)
-                        .replace('{{__profile_name__}}', doc.data().name)
-                        .replace('{{__profile_position__}}',doc.data().position)
-                        .replace('{{__profile_github__}}',doc.data().github)
-                        .replace('{{__profile_email__}}',doc.data().email)
-                        .replace('{{__profile_introduce__}}',doc.data().introduce);
-    newProfile.innerHTML=template;
+    newProfileIndex.setAttribute("id", `item${itemNumber}`);
+    newProfileIndex.classList.add("detail-index");
+    newProfileIndex.innerHTML = `#${itemNumber + 1}`;
+
+    template = template
+      .replace("{{__profile_image__}}", doc.data().image)
+      .replace("{{__profile_name__}}", doc.data().name)
+      .replace("{{__profile_position__}}", doc.data().position)
+      .replace("{{__profile_github__}}", doc.data().github)
+      .replace("{{__profile_email__}}", doc.data().email)
+      .replace("{{__profile_introduce__}}", doc.data().introduce);
+    newProfile.innerHTML = template;
     profileContainer.append(newProfileIndex);
     profileContainer.append(newProfile);
-    template = template.replace(doc.data().image,'{{__profile_image__}}')
-                        .replace(doc.data().name, '{{__profile_name__}}')
-                        .replace(doc.data().position,'{{__profile_position__}}')
-                        .replace(doc.data().github,'{{__profile_github__}}')
-                        .replace(doc.data().email,'{{__profile_email__}}')
-                        .replace(doc.data().introduce,'{{__profile_introduce__}}');
+
+    template = template
+      .replace(doc.data().image, "{{__profile_image__}}")
+      .replace(doc.data().name, "{{__profile_name__}}")
+      .replace(doc.data().position, "{{__profile_position__}}")
+      .replace(doc.data().github, "{{__profile_github__}}")
+      .replace(doc.data().email, "{{__profile_email__}}")
+      .replace(doc.data().introduce, "{{__profile_introduce__}}");
     itemNumber++;
 
     //삭제 버튼 생성
     const deleteBtn = document.createElement("button");
-    deleteBtn.classList.add("detail-button")
+    deleteBtn.classList.add("detail-button");
     deleteBtn.textContent = "❌ 삭제";
     deleteEvent(deleteBtn, doc);
     newProfile.prepend(deleteBtn);
 
     //상단 수정 버튼
     const modifyBtn = document.createElement("button");
-    modifyBtn.classList.add("detail-button")
+    modifyBtn.classList.add("detail-button");
     modifyBtn.textContent = "✏️ 편집";
-    modifyBtn.addEventListener("click",async()=>{
+    modifyBtn.addEventListener("click", async () => {
       const textContainer = newProfile.querySelector(".detail-text-container");
       const subtitleSpans = textContainer.querySelectorAll(".subtitle");
-    
+
       //하단 이미지 수정 버튼 생성
       const imgModifyBtn = document.createElement("input");
       imgModifyBtn.type = "file";
-      imgModifyBtn.accept="image/*";
-      imgModifyBtn.setAttribute("id","btn__img-modify")
+      imgModifyBtn.accept = "image/*";
+      imgModifyBtn.setAttribute("id", "btn__img-modify");
       const imgModifyBtnLabel = document.createElement("label");
-      imgModifyBtnLabel.setAttribute("for","btn__img-modify");
-      imgModifyBtnLabel.innerText="Change Image";
-      imgModifyBtn.style.display="none";
-      imgModifyBtnLabel.style.display="none";
+      imgModifyBtnLabel.setAttribute("for", "btn__img-modify");
+      imgModifyBtnLabel.innerText = "Change Image";
+      imgModifyBtn.style.display = "none";
+      imgModifyBtnLabel.style.display = "none";
       newProfile.insertAdjacentElement("afterend", imgModifyBtnLabel);
       newProfile.insertAdjacentElement("afterend", imgModifyBtn);
 
       const profileId = newProfile.getAttribute("id");
-      const detailProfileImg = newProfile.querySelector('.detail-profile-image')
+      const detailProfileImg = newProfile.querySelector(
+        ".detail-profile-image"
+      );
 
-      imgModifyBtn.addEventListener("change", async() => {
+      imgModifyBtn.addEventListener("change", async () => {
         const selectedFile = imgModifyBtn.files[0];
         if (selectedFile) {
-          const newImagestorageRef = ref(storage, 'image/'+Timestamp.fromDate(new Date())+selectedFile.name);
+          const newImagestorageRef = ref(
+            storage,
+            "image/" + Timestamp.fromDate(new Date()) + selectedFile.name
+          );
           await uploadBytes(newImagestorageRef, selectedFile);
           let url = await getDownloadURL(newImagestorageRef);
-          let newToSave = {image: url};
-          
+          let newToSave = { image: url };
+
           //기존 이미지 삭제
-          let imageRef = ref(storage, ref(doc.data().image))
+          let imageRef = ref(storage, ref(doc.data().image));
           deleteObject(imageRef);
           //preview 띄워주기 및 업로드
           detailProfileImg.src = url;
@@ -118,47 +144,47 @@ onSnapshot(q,(querySnapshot) => {
       //하단 이미지 삭제 버튼 생성 및 동작
       const imgDeleteBtn = document.createElement("button");
       imgDeleteBtn.innerText = "Delete Image";
-      imgDeleteBtn.setAttribute("id","btn__img-delete");
-      imgDeleteBtn.style.display="none";
+      imgDeleteBtn.setAttribute("id", "btn__img-delete");
+      imgDeleteBtn.style.display = "none";
       newProfile.insertAdjacentElement("afterend", imgDeleteBtn);
-      imgDeleteBtn.addEventListener("click", ()=>{
+      imgDeleteBtn.addEventListener("click", () => {
         detailProfileImg.src = "../../assets/images/human_icon.png";
         //기존 이미지 삭제
-        let imageRef = ref(storage, ref(doc.data().image))
+        let imageRef = ref(storage, ref(doc.data().image));
         deleteObject(imageRef);
         //db업데이트
-        const noImage = {image: "../../assets/images/human_icon.png"};
+        const noImage = { image: "../../assets/images/human_icon.png" };
         imageUpdate(profileId, noImage);
-      })
+      });
 
       //상단 수정 버튼 동작 내용
       clickCount++;
-      switch(clickCount%3){
+      switch (clickCount % 3) {
         //이미지 수정 및 삭제 관련
         case 1:
-          imgModifyBtnLabel.style.display="inline";
-          imgDeleteBtn.style.display="inline";
+          imgModifyBtnLabel.style.display = "inline";
+          imgDeleteBtn.style.display = "inline";
           modifyBtn.textContent = "✏️ 편집중 : 사진";
           break;
         //텍스트 수정 관련
         case 2:
           modifyBtn.textContent = "✏️ 편집중 : 텍스트";
-          const imgModifyBtnLabels = document.querySelectorAll('label')
-          imgModifyBtnLabels.forEach(item=>{
-            item.style.display="none";
-          })
-          const imgDeleteBtns = document.querySelectorAll('#btn__img-delete')
-          imgDeleteBtns.forEach(item=>{
-            item.style.display="none";
-          })
-           //수정할 수 있는 input 만들기
-           subtitleSpans.forEach((subtitle,index) => {
+          const imgModifyBtnLabels = document.querySelectorAll("label");
+          imgModifyBtnLabels.forEach((item) => {
+            item.style.display = "none";
+          });
+          const imgDeleteBtns = document.querySelectorAll("#btn__img-delete");
+          imgDeleteBtns.forEach((item) => {
+            item.style.display = "none";
+          });
+          //수정할 수 있는 input 만들기
+          subtitleSpans.forEach((subtitle, index) => {
             const input = document.createElement("input");
             const innerTextSpan = subtitle.nextElementSibling;
             input.value = innerTextSpan.textContent;
             subtitle.insertAdjacentElement("afterend", input);
             innerTextSpan.style.display = "none";
-            });
+          });
           break;
         //업로드 관련
         case 0:
@@ -168,15 +194,15 @@ onSnapshot(q,(querySnapshot) => {
 
           // 각 subtitle에 대한 수정 내용을 updatedData 객체에 추가
           subtitleSpans.forEach((subtitle, index) => {
-          const input = subtitle.nextElementSibling;
-          const innerTextSpan = input.nextElementSibling;
+            const input = subtitle.nextElementSibling;
+            const innerTextSpan = input.nextElementSibling;
 
-          innerTextSpan.textContent = input.value;
-          innerTextSpan.style.display = "inline";
-          input.remove();
-      
-          const fieldName = subtitle.textContent.slice(3).toLowerCase();
-          updatedData[fieldName] = input.value;
+            innerTextSpan.textContent = input.value;
+            innerTextSpan.style.display = "inline";
+            input.remove();
+
+            const fieldName = subtitle.textContent.slice(3).toLowerCase();
+            updatedData[fieldName] = input.value;
           });
           dbUpdate(profileId, updatedData);
           break;
@@ -189,7 +215,7 @@ onSnapshot(q,(querySnapshot) => {
     // 초기 로딩 시 해시값에 따른 초기 상태 설정
     handleHashChange();
   });
-})
+});
 
 function handleHashChange() {
   const hash = location.hash;
@@ -199,7 +225,6 @@ function handleHashChange() {
     targetItem.scrollIntoView({ behavior: "smooth", block: "start" });
   }
 }
-
 
 function deleteDocument(documentId) {
   const documentRef = doc(db, "profiles", documentId);
@@ -214,31 +239,31 @@ function deleteDocument(documentId) {
     });
 }
 
-function deleteEvent(target, doc){
-  target.addEventListener("click", ()=>{
+function deleteEvent(target, doc) {
+  target.addEventListener("click", () => {
     deleteDocument(doc.id);
-    let imageRef = ref(storage, ref(doc.data().image))
+    let imageRef = ref(storage, ref(doc.data().image));
     deleteObject(imageRef);
   });
 }
 
-async function dbUpdate (profileId, updatedData){
-  try{
-    const docRef = doc(db,"profiles",profileId)
-    await updateDoc(docRef,updatedData);
-    alert("수정 완료!")
+async function dbUpdate(profileId, updatedData) {
+  try {
+    const docRef = doc(db, "profiles", profileId);
+    await updateDoc(docRef, updatedData);
+    alert("수정 완료!");
     // window.location.reload();
-    window.location.href="/pages/moreInfo/moreInfo.html";
-  }catch(error){
-    console.log("오류발생",error)
+    window.location.href = "/pages/moreInfo/moreInfo.html";
+  } catch (error) {
+    console.log("오류발생", error);
   }
 }
 
-async function imageUpdate(profileId, newToSave){
-  try{
-    const docRef = doc(db,"profiles",profileId)
-    await updateDoc(docRef,newToSave);
-  }catch(error){
-    console.log('이미지수정 오류발생',error)
+async function imageUpdate(profileId, newToSave) {
+  try {
+    const docRef = doc(db, "profiles", profileId);
+    await updateDoc(docRef, newToSave);
+  } catch (error) {
+    console.log("이미지수정 오류발생", error);
   }
 }
--- pages/playground/playground.js
+++ pages/playground/playground.js
@@ -2,9 +2,10 @@ const interactionBox = document.querySelector(".interaction");
 
 let i = 0;
 let change = 0;
-window.addEventListener('click',()=>{
+//클릭 시 색깔 변경
+window.addEventListener("click", () => {
   i++;
-  switch(i%4){
+  switch (i % 4) {
     case 0:
       change = 0;
       break;
@@ -18,27 +19,26 @@ window.addEventListener('click',()=>{
       change = 300;
       break;
   }
-})
-
-function createParticle (x, y) {
-
+});
+//파티클 생성 함수
+function createParticle(x, y) {
   let size = Math.random() * 35;
-  
-  x -= (size / 2);
-  y -= (size / 2);
-  
-  let particle = document.createElement('div');
-  particle.classList.add('particle');
+  //파티클 중심이 커서에 위치하도록
+  x -= size / 2;
+  y -= size / 2;
+
+  let particle = document.createElement("div");
+  particle.classList.add("particle");
   interactionBox.appendChild(particle);
-  
+  //초기 파티클 TweenMax.set(element, {property: value});
   TweenMax.set(particle, {
-    x: x, 
+    x: x,
     y: y,
     width: size,
     height: size,
-    background: bgColor(change)
+    background: bgColor(change),
   });
- 
+  //애니메이션 TweenMax.to(element, duration, {property:value});
   TweenMax.to(particle, Math.random() * 15 + 1, {
     x: x + (Math.random() - 0.5) * 200,
     y: y + (Math.random() - 0.5) * 200,
@@ -47,26 +47,37 @@ function createParticle (x, y) {
     ease: Power2.easeOut,
     onComplete: function () {
       interactionBox.removeChild(particle);
-    }
+    },
   });
 }
+//최적화
+const throttledCreateParticle = _.throttle(createParticle, 15);
 
-const throttledCreateParticle = _.throttle(createParticle,15);
+//커서에 따라 파티클 생성 (PC)
+window.addEventListener(
+  "mousemove",
+  function (e) {
+    //현재 커서 위치
+    let x = e.clientX;
+    let y = e.clientY;
+    throttledCreateParticle(x, y);
+  },
+  { passive: true }
+);
 
-window.addEventListener('mousemove', function (e) {
-  var x = e.clientX;
-  var y = e.clientY;
-  throttledCreateParticle(x, y);
-}, { passive: true });
-
-interactionBox.addEventListener('touchmove', function (e) {
-  var x = e.touches[0].clientX;
-  var y = e.touches[0].clientY;
-  e.preventDefault();
-  throttledCreateParticle(x, y);
-}, { passive: true });
-
-
-function bgColor (change) {
-  return `hsl(${Math.random() *90+change}, 50%, 70%)`
+//터치에 따라 파티클 생성 (모바일)
+interactionBox.addEventListener(
+  "touchmove",
+  function (e) {
+    //현재 터치 위치
+    let x = e.touches[0].clientX;
+    let y = e.touches[0].clientY;
+    e.preventDefault();
+    throttledCreateParticle(x, y);
+  },
+  { passive: true }
+);
+//비슷한 계열의 색깔이 랜덤하게 나오게 해주는 함수
+function bgColor(change) {
+  return `hsl(${Math.random() * 90 + change}, 50%, 70%)`;
 }
