2459bd2efa52cd1fad778f4aa3806872d2b2c3f0
MinSeob <icdmiracle927@gmail.com>
2023-08-28 14:39:26 +0900

    Fix : FirstPage.js tagName -> li, li요소들 반복문으로 생성

--- src/components/FirstPage.js
+++ src/components/FirstPage.js
@@ -9,79 +9,95 @@ export default class Firstpage extends Component {
                 <p class="first__page--title">VALLEY KOREA</p>
             </div>
             <ul class="first__page--list">
-                <div class="fist-space space">아래로 스크롤 해보세요</div>
-                <li class="first__page--list-item">✨서울</li>
-                <li class="first__page--list-item">🐟대전</li>
-                <li class="first__page--list-item">✅대구</li>
-                <li class="first__page--list-item">😲부산</li>
-                <li class="first__page--list-item">✈️강원도</li>
-                <li class="first__page--list-item">🏳️‍⚧️충청남도</li>
-                <li class="first__page--list-item">🏠충청북도</li>
-                <li class="first__page--list-item">☕경상남도</li>
-                <li class="first__page--list-item">📸경상북도</li>
-                <li class="first__page--list-item">🚗경기도</li>
-                <li class="first__page--list-item">✨대한민국방방곳곳</li>
-                <li class="first__page--list-item">🐟아름다운계곡들</li>
-                <li class="first__page--list-item">✅남이섬계곡</li>
-                <li class="first__page--list-item">😲삼학사계곡</li>
-                <li class="first__page--list-item">✈️청계천</li>
-                <li class="first__page--list-item">🏳️‍⚧️설악산계곡</li>
-                <li class="first__page--list-item">🏠월정사계곡</li>
-                <li class="first__page--list-item">☕대청호계곡</li>
-                <li class="first__page--list-item">📸소백산계곡</li>
-                <li class="first__page--list-item">🚗영종도계곡</li>
-                <li class="first__page--list-item">✨지리산계곡</li>
-                <li class="first__page--list-item">🐟뱀삿골계곡</li>
-                <li class="first__page--list-item">✅백운계곡</li>
-                <li class="first__page--list-item">😲구례강서계곡</li>
-                <li class="first__page--list-item">✈️삼척강촌계곡</li>
-                <li class="first__page--list-item">🏳️‍⚧️오대산계곡</li>
-                <li class="first__page--list-item">🏠무등산계곡</li>
-                <li class="first__page--list-item">☕경주안강계곡</li>
-                <li class="first__page--list-item">📸강릉송정계곡</li>
-                <li class="first__page--list-item">🚗홍천청평계곡</li>
-                <li class="first__page--list-item">✨홍천군노고단계곡</li>
-                <li class="first__page--list-item">🐟원주시함박계곡</li>
-                <li class="first__page--list-item">✅무릉계곡</li>
-                <li class="first__page--list-item">😲양평계곡</li>
-                <li class="first__page--list-item">✈️밀양시밀양천곡</li>
-                <li class="first__page--list-item">🏳️‍⚧️문경강곡</li>
-                <li class="first__page--list-item">🏠인제임동계곡</li>
-                <li class="first__page--list-item">☕곡성금수산계곡</li>
-                <li class="first__page--list-item">📸춘천두물머리계곡</li>
-                <li class="first__page--list-item">🚗양양임나무계곡</li>
-                <li class="first__page--list-item">✨구곡계곡</li>
-                <li class="first__page--list-item">✅보령소백산맥계곡</li>
-                <li class="first__page--list-item">🐟임실군대야산계곡</li>
-                <li class="first__page--list-item">😲안동동호천계곡</li>
-                <li class="first__page--list-item">✈️대전계룡산국립공원계곡</li>
-                <li class="first__page--list-item">🏳️‍⚧️군산천마산계곡</li>
-                <li class="first__page--list-item">🏠선유도계곡</li>
-                <li class="first__page--list-item">☕장군봉계곡</li>
-                <li class="first__page--list-item">📸남해독일마을계곡</li>
-                <li class="first__page--list-item">🚗순천우포늪계곡</li>
-                <li class="first__page--list-item">✨가라산계곡</li>
-                <li class="first__page--list-item">🐟강진강계곡</li>
-                <li class="first__page--list-item">✅목포백제해수욕장계곡</li>
-                <li class="first__page--list-item">😲외암골계곡</li>
-                <li class="first__page--list-item">✈️홍천우레골계곡</li>
-                <li class="first__page--list-item">🏳️‍⚧️인동계곡</li>
-                <li class="first__page--list-item">🏠고성미타리계곡</li>
-                <li class="first__page--list-item">☕마로길계곡</li>
-                <li class="first__page--list-item">📸인제상봉골계곡</li>
-                <li class="first__page--list-item">🚗가평용소계곡</li>
-                <div class="last-space space">
+                <li id="next_add" class="fist-space space">아래로 스크롤 해보세요</li>
+                <li class="last-space space">
                     <button class="enter__button">ENTER</button>
-                </div>
-                
+                </li>           
             </ul>
         </div>
     `;
         this.el.classList.add('first__page');
 
         //intersection observer
-        const target = this.el.querySelectorAll('.first__page--list-item');
         const targeter = this.el.querySelector('.first__page--list');
+        const targeterSpace = this.el.querySelector('#next_add');
+
+        const iconList = ['✨', '🐟', '✅', '😲', '✈️', '🏳️‍⚧️', '🏠', '☕', '📸', '🚗'].reverse();
+        const itemList = [
+            '서울',
+            '대전',
+            '대구',
+            '부산',
+            '강원도',
+            '충청남도',
+            '충청북도',
+            '경상남도',
+            '경상북도',
+            '경기도',
+            '대한민국방방곳곳',
+            '아름다운계곡들',
+            '남이섬계곡',
+            '삼학사계곡',
+            '청계천',
+            '설악산계곡',
+            '월정사계곡',
+            '대청호계곡',
+            '소백산계곡',
+            '영종도계곡',
+            '지리산계곡',
+            '뱀삿골계곡',
+            '백운계곡',
+            '구례강서계곡',
+            '삼척강촌계곡',
+            '오대산계곡',
+            '무등산계곡',
+            '경주안강계곡',
+            '강릉송정계곡',
+            '홍천청평계곡',
+            '홍천군노고단계곡',
+            '원주시함박계곡',
+            '무릉계곡',
+            '양평계곡',
+            '밀양시밀양천곡',
+            '문경강곡',
+            '인제임동계곡',
+            '곡성금수산계곡',
+            '춘천두물머리계곡',
+            '양양임나무계곡',
+            '구곡계곡',
+            '보령소백산맥계곡',
+            '임실군대야산계곡',
+            '안동동호천계곡',
+            '대전계룡산국립공원계곡',
+            '군산천마산계곡',
+            '선유도계곡',
+            '장군봉계곡',
+            '남해독일마을계곡',
+            '순천우포늪계곡',
+            '가라산계곡',
+            '강진강계곡',
+            '목포백제해수욕장계곡',
+            '외암골계곡',
+            '홍천우레골계곡',
+            '인동계곡',
+            '고성미타리계곡',
+            '마로길계곡',
+            '인제상봉골계곡',
+            '가평용소계곡',
+        ];
+
+        (function createLiItems() {
+            itemList.reverse().forEach((item, index) => {
+                const newLiItem = document.createElement('li');
+                newLiItem.classList.add('first__page--list-item');
+                const iconNumber = Math.floor(index % 10);
+                console.log(iconNumber);
+                newLiItem.innerText = `${iconList[iconNumber]}` + item;
+                targeter.insertBefore(newLiItem, targeterSpace.nextSibling);
+            });
+        })();
+
+        const target = this.el.querySelectorAll('.first__page--list-item');
 
         function createObserver(threshold) {
             const options = {
@@ -113,13 +129,13 @@ export default class Firstpage extends Component {
                 const x = 0.05;
                 createObserver(x);
             } else if (windowWidth > 600) {
-                const x = 0.4;
+                const x = 0.1;
                 createObserver(x);
             } else if (windowWidth > 400) {
-                const x = 0.5;
+                const x = 0.2;
                 createObserver(x);
             } else {
-                const x = 0.7;
+                const x = 0.3;
                 createObserver(x);
             }
         }
