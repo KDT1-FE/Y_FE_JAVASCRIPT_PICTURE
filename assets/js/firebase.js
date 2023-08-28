fe5a076e7381c25966cae686661fbf566a0ea9f5
mysdpy30s <mysdpy30s@gmail.com>
2023-08-29 01:13:45 +0900

    Update Final

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -20,6 +20,15 @@ import {
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.FIREBASE_APP_ID,
+// };
+
 const firebaseConfig = {
   apiKey: "AIzaSyAaOxlvdJlsMMUZHDi325Y4SOUI2-wvO6U",
   authDomain: "myplant-21025.firebaseapp.com",
