074c5d3913e13c49b8a01c418021fd51c408499b
dbstjrals <soekmin1998@naver.com>
2023-08-28 17:08:39 +0900

    Remove: 불필요한 js 파일 삭제

--- public/data.js
+++
@@ -1,7 +0,0 @@
-const db = firebase.firestore();
-// const soldierDummy = []
-// db.collection('user').get().then((snapshot) => {
-//     snapshot.forEach((doc) => {
-//         soldierDummy.push(doc.data())
-//     });
-// });
