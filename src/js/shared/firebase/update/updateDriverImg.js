e1137aa1b2ddcc728608e0f311eea20cde74f506
JitHoon <imessi0336@gmail.com>
2023-08-28 17:28:32 +0900

    Refactor: Clean updateImg code

--- src/js/shared/firebase/update/updateDriverImg.js
+++ src/js/shared/firebase/update/updateDriverImg.js
@@ -3,12 +3,9 @@ import { doc, updateDoc } from "firebase/firestore";
 
 export async function updateDriverImg(id, imgUrl) {
   try {
-    // 사용자가 업데이트한 이미지가 있으면 사진 없데이트 진행 x
-    if (id && imgUrl) {
-      await updateDoc(doc(db, "drivers", `${id}`), {
-        imgUrl
-      });
-    } else throw "404 페이지로";
+    await updateDoc(doc(db, "drivers", `${id}`), {
+      imgUrl
+    });
   } catch (err) {
     console.log(err);
   }
