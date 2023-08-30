71ad113fff3f24fe06710d110929cfdb331baac9
89882 <pgh123166@naver.com>
2023-08-30 16:01:17 +0900

    refactor: 404 component 구조 변경

--- src/routes/NotFound.js
+++ src/routes/NotFound.js
@@ -5,12 +5,13 @@ export default class NotFound extends Component {
   constructor() {
     super({ attributes: { class: 'not-found' } });
   }
-  render() {
-    this.componentRoot.innerHTML = `요청하신 페이지를 찾을 수 없습니다<button class="not-found-button">홈으로 돌아가기 </button>`;
-
-    const notFoundButton =
-      this.componentRoot.querySelector('.not-found-button');
-    notFoundButton.addEventListener('click', () => {
+  template() {
+    return `
+    요청하신 페이지를 찾을 수 없습니다<button class="not-found-button">홈으로 돌아가기 </button>
+    `;
+  }
+  setEvent() {
+    this.addEvent('click', '.not-found-button', () => {
       navigate();
     });
   }
