168b07e33f31fb084430e116b692a323a67ae7d6
89882 <pgh123166@naver.com>
2023-08-30 16:00:19 +0900

    refactor: member component 구조 변경

--- src/components/Member.js
+++ src/components/Member.js
@@ -5,23 +5,22 @@ export default class Member extends Component {
   constructor(props) {
     super({ props, tagName: 'a', attributes: { class: 'member' } });
   }
-  render() {
+  template() {
     const { member } = this.props;
-
     this.componentRoot.setAttribute('href', `/#/detail?id=${member.id}`);
-    this.componentRoot.innerHTML =
-      /* html */
-      `
-      <div class="row-member">  <div class='checkbox-container'>
-      <input class='checkbox' type='checkbox'></input>
-    </div>
-    <p class='photo' style="background-image: url(${member.photoUrl})"></p>
-    <p class="name-title">${member.name}</p>
-    <p class="email-title">${member.email}</p></div>
+    return `
+    <div class="row-member">  <div class='checkbox-container'>
+    <input class='checkbox' type='checkbox'></input>
+  </div>
+  <p class='photo' style="background-image: url(${member.photoUrl})"></p>
+  <p class="name-title">${member.name}</p>
+  <p class="email-title">${member.email}</p></div>
     `;
+  }
 
-    const checkBox = this.componentRoot.querySelector('.checkbox');
-    checkBox.addEventListener('change', (event) => {
+  setEvent() {
+    const { member } = this.props;
+    this.addEvent('change', '.checkbox', (event) => {
       if (event.currentTarget.checked) {
         memberStore.state.deleteMembers.push({
           id: member.id,
