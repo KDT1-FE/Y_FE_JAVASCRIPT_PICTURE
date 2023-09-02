f288f505192151d93271d3e5b40aef7c0a00f570
TaePoong719 <just5538@gmail.com>
2023-09-02 13:18:59 +0900

    Fix: 뒤로가기나 메인으로 돌아갈 때 무한 스크롤 되지 않는 오류 수정

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,9 +3,11 @@ import Headline from "../components/Headline";
 import Search from "../components/Search";
 import ChampionList from "../components/ChampionList";
 import { initializeChampionList } from "../store/champion";
+import championStore from "../store/champion";
 
 export default class Home extends Component{
   render(){
+    championStore.state.page = 1
     const headline = new Headline().el
     const search = new Search().el
     const championlist = new ChampionList().el
