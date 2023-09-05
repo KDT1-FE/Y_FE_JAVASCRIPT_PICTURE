0994ed4e39e64e04a20eba81375d87881d48bbd5
suehub <dldustn2040@gmail.com>
2023-09-05 20:24:11 +0900

    Delete keys.js

--- src/js/keys.js
+++
@@ -1,9 +0,0 @@
-export const apis = {
-  API_KEY: "AIzaSyA0BYrAIfJUyc09tEbBSZkRmDW6pRerOG8",
-  AUTH_DOMAIN: "employee-management-59973.firebaseapp.com",
-  PROJECT_ID: "employee-management-59973",
-  STORAGE_BUCKET: "employee-management-59973.appspot.com",
-  SENDER_ID: "541264054170",
-  APP_ID: "1:541264054170:web:2e032d0b49e158016f9c23",
-  MEASUREMENT_ID: "G-WFS0KJ2Y3C",
-};
